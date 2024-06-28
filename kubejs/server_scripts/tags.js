c988c4f81245d06ed7ffcfd0e287ff444f886799
Phantom <33400200+WhitePhant0m@users.noreply.github.com>
2024-06-28 23:53:43 +0300

    Update tags.js
    Fix ae2 wrenches not working as wrenches for other mods, and other mods not recognizing them as wrenches.

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -1,3 +1,10 @@
-ServerEvents.tags('block', e => {
-  e.add('ftbchunks:interact_whitelist', ['@waystones']);
+ServerEvents.tags("item", e => {
+  //Fixes ae2 wrenches not working as wrenches for other mods, and other mods not recognizing them as wrenches.
+  //Should be removed when AE2 fixes this on their end.
+  e.add("c:tools/wrench", ["#ae2:quartz_wrench", "ae2:network_tool"]);
+  e.add("c:tools/wrenches", "#c:wrenches");
+});
+
+ServerEvents.tags("block", e => {
+  e.add("ftbchunks:interact_whitelist", ["@waystones"]);
 });
