7d79d79fa2fc79c2aea8bd0920e10b210ac4d56a
WhitePhant0m <kallunki.aleksi@gmail.com>
2024-07-19 19:05:17 +0300

    min/max spawn delay to 100, spawn count to 8

--- kubejs/server_scripts/Mods/Apothic/Spawners.js
+++ kubejs/server_scripts/Mods/Apothic/Spawners.js
@@ -32,18 +32,20 @@ ServerEvents.recipes(e => {
       type: `apothic_spawners:${type}`,
       value: main.value
     });
-    if (main.limit.includes("min:"))
-      recipe.stat_changes[0].min = parseInt(main.limit.split(":")[1]);
-    else
-      recipe.stat_changes[0].max = parseInt(main.limit.split(":")[1]);
+    if (main.limit)
+      if (main.limit.includes("min:"))
+        recipe.stat_changes[0].min = parseInt(main.limit.split(":")[1]);
+      else
+        recipe.stat_changes[0].max = parseInt(main.limit.split(":")[1]);
     recipeInverse.stat_changes.push({
       type: `apothic_spawners:${type}`,
       value: inverse.value
     });
-    if (inverse.limit.includes("min:"))
-      recipeInverse.stat_changes[0].min = parseInt(inverse.limit.split(":")[1]);
-    else
-      recipeInverse.stat_changes[0].max = parseInt(inverse.limit.split(":")[1]);
+    if (inverse.limit)
+      if (inverse.limit.includes("min:"))
+        recipeInverse.stat_changes[0].min = parseInt(inverse.limit.split(":")[1]);
+      else
+        recipeInverse.stat_changes[0].max = parseInt(inverse.limit.split(":")[1]);
 
     e.remove({ id: `apothic_spawners:spawner_modifiers/${type}` });
     e.remove({ id: `apothic_spawners:spawner_modifiers/_inverse/${type}` });
@@ -54,4 +56,8 @@ ServerEvents.recipes(e => {
   // Examples, works with either tags or items
   // modifier("#c:ingots/iron", { value: 10, limit: "min:10" }, { value: -10, limit: "max:10" }, "min_delay");
   // modifier("#c:gems/diamond", { value: 10, limit: "min:10" }, { value: -10, limit: "max:10" }, "max_delay");
+
+  modifier("minecraft:sugar", { value: -10, limit: "min:100" }, { value: 10, limit: "max:1600" }, "min_delay");
+  modifier("minecraft:clock", { value: -10, limit: "min:100" }, { value: 10, limit: "max:1600" }, "max_delay");
+  modifier("minecraft:fermented_spider_eye", { value: 1, limit: "max:8" }, { value: -1, limit: "min:1" }, "spawn_count");
 });
