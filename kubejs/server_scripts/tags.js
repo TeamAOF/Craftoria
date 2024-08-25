d064c94c886c9f1e4cb479ac4392cf714d58d967
Phantom <33400200+WhitePhant0m@users.noreply.github.com>
2024-08-25 12:54:26 +0300

    Tags
    Added all Chipped glass to mineable/pickaxe tag,
    Add a few MI fluids to fluid tags that PNC uses,
    Add `#c:budding_blocks` -> `#c:budding` (ae2 uses this tag for growth accel)
    Add simple magnets to curio tag.

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -1,36 +1,48 @@
-ServerEvents.tags("item", e => {
-  //Fixes ae2 wrenches not working as wrenches for other mods, and other mods not recognizing them as wrenches.
-  //Should be removed when AE2 fixes this on their end.
-  e.add("c:tools/wrench", ["#ae2:quartz_wrench", "ae2:network_tool"]);
-  e.add("c:tools/wrenches", "#c:wrenches");
-  e.remove("c:dyes/yellow", "mekanism:dust_sulfur");
+ServerEvents.tags('item', (e) => {
+  e.add('minecraft:storage_blocks/quartz', 'minecraft:quartz_block');
+  e.add('c:storage_blocks/amethyst', 'minecraft:amethyst_block');
+  e.add('c:storage_blocks/fluix', 'ae2:fluix_block');
+  e.add('c:storage_blocks/charged_redstone', 'appflux:charged_redstone_block');
 
-  e.add("minecraft:storage_blocks/quartz", "minecraft:quartz_block");
-  e.add("c:storage_blocks/amethyst", "minecraft:amethyst_block");
-  e.add("c:storage_blocks/fluix", "ae2:fluix_block");
-  e.add("c:storage_blocks/charged_redstone", "appflux:charged_redstone_block");
+  e.add('curios:curio', ['simplemagnets:basicmagnet', 'simplemagnets:advancedmagnet']);
 
   // Crops & Seeds
-  e.add("c:seeds/cabbage", "farmersdelight:cabbage_seeds");
-  e.add("c:seeds/tomato", "farmersdelight:tomato_seeds");
-  e.add("c:seeds/rice", "sushigocrafting:rice_seeds");
+  e.add('c:seeds/cabbage', 'farmersdelight:cabbage_seeds');
+  e.add('c:seeds/tomato', 'farmersdelight:tomato_seeds');
+  e.add('c:seeds/rice', 'sushigocrafting:rice_seeds');
 });
 
 ServerEvents.tags('block', (e) => {
-	e.add('ftbchunks:interact_whitelist', ['@waystones']);
+  e.add('ftbchunks:interact_whitelist', ['@waystones']);
 
-	e.add('minecraft:mineable/pickaxe', [
-		'#c:glass_blocks',
-		'#c:glass_panes',
-		'#chipped:glowstone',
-		/^mekanism:(basic|advanced|elite|ultimate)_(universal_cable|mechanical_pipe|pressurized_tube|logistical_transporter|thermodynamic_conductor)$/,
-		'mekanism:diversion_transporter',
-		'mekanism:restrictive_transporter',
-	]);
+  let colors = ['white', 'orange', 'magenta', 'light_blue', 'yellow', 'lime', 'pink', 'gray', 'light_gray', 'cyan', 'purple', 'blue', 'brown', 'green', 'red', 'black'];
 
-  e.add("minecraft:mineable/axe", [
-    "mekanism:cardboard_box",
+  colors.forEach((color) => {
+    e.add('c:glass_blocks', `#chipped:${color}_stained_glass`);
+    e.add('c:glass_panes', `#chipped:${color}_stained_glass_pane`);
+  });
+
+  e.add('c:glass_blocks', ['#chipped:glass']);
+  e.add('c:glass_panes', ['#chipped:glass_pane']);
+
+  e.add('minecraft:mineable/pickaxe', [
+    '#c:glass_blocks',
+    '#c:glass_panes',
+    '#chipped:glowstone',
+    /^mekanism:(basic|advanced|elite|ultimate)_(universal_cable|mechanical_pipe|pressurized_tube|logistical_transporter|thermodynamic_conductor)$/,
+    'mekanism:diversion_transporter',
+    'mekanism:restrictive_transporter',
   ]);
 
-  e.add("minecraft:storage_blocks/quartz", "minecraft:quartz_block");
+  e.add('minecraft:mineable/axe', ['mekanism:cardboard_box']);
+
+  e.add('minecraft:storage_blocks/quartz', 'minecraft:quartz_block');
+
+  e.add('c:budding', '#c:budding_blocks');
+});
+
+ServerEvents.tags('fluid', (e) => {
+  e.add('c:lubricant', 'modern_industrialization:lubricant');
+  e.add('c:plantoil', 'modern_industrialization:plant_oil');
+  e.add('c:fuels/crude_oil', 'modern_industrialization:crude_oil');
 });
