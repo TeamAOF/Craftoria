8523eb882e214978db486a85b66e2d058f4f90de
WhitePhant0m <kallunki.aleksi@gmail.com>
2024-09-11 20:00:06 +0300

    Removals via output now also hides said items. No manual hiding needed.

--- kubejs/server_scripts/removals.js
+++ kubejs/server_scripts/removals.js
@@ -1,28 +1,28 @@
+const globalItemRemovals = [
+  'megacells:mega_interface',
+  'megacells:cable_mega_interface',
+  'megacells:mega_pattern_provider',
+  'megacells:cable_mega_pattern_provider',
+  'bigger_ae2:advanced_item_cell_housing',
+  'bigger_ae2:quantum_item_storage_cell',
+  'bigger_ae2:digital_singularity_item_storage_cell',
+  'chisel:chisel',
+];
+
 ServerEvents.recipes((event) => {
-
   const id = [
-    "appflux:inscriber/crush_diamond",
-    "appflux:inscriber/crush_emerald",
-    "modern_industrialization:electric_age/machine/assembler/replicator",
-    "mffs:steel_compound",
-    "mffs:steel_ingot",
-  ];
-
-  const output = [
-    "megacells:mega_interface",
-    "megacells:cable_mega_interface",
-    "megacells:mega_pattern_provider",
-    "megacells:cable_mega_pattern_provider",
-    "bigger_ae2:advanced_item_cell_housing",
-    "bigger_ae2:quantum_item_storage_cell",
-    "bigger_ae2:digital_singularity_item_storage_cell",
+    'appflux:inscriber/crush_diamond',
+    'appflux:inscriber/crush_emerald',
+    'modern_industrialization:electric_age/machine/assembler/replicator',
+    'mffs:steel_compound',
+    'mffs:steel_ingot',
   ];
 
   id.forEach((id) => {
     event.remove({ id: id });
   });
 
-  output.forEach((output) => {
+  globalItemRemovals.forEach((output) => {
     event.remove({ output: output });
   });
 });
