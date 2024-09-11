bc6a9f4bf3fcbec350919b0ed731c364161a6bea
WhitePhant0m <kallunki.aleksi@gmail.com>
2024-09-11 21:21:32 +0300

    Clean up files

--- kubejs/server_scripts/Mods/Craftoria/ConflictFixes.js
+++ kubejs/server_scripts/Mods/Craftoria/ConflictFixes.js
@@ -1,12 +1,41 @@
 ServerEvents.recipes((e) => {
-	// Fixes Handcrafted Cupboards and Functional Storage 1x1 Drawers conflicting
-	Ingredient.of('#handcrafted:cupboards').itemIds.forEach((item) => {
-		e.remove({ output: item });
-		let item2 = item.split(':')[1].replace('_cupboard', '');
-		e.shaped(`3x ${item}`, ['AAA', 'BCB', 'AAA'], {
-			A: `minecraft:${item2}_planks`,
-			B: `minecraft:${item2}_slab`,
-			C: '#c:chests',
-		}).id(`craftoria:handcrafted/${item2}_cupboard`);
-	});
+  let colors = [
+    'white',
+    'orange',
+    'magenta',
+    'light_blue',
+    'yellow',
+    'lime',
+    'pink',
+    'gray',
+    'light_gray',
+    'cyan',
+    'purple',
+    'blue',
+    'brown',
+    'green',
+    'red',
+    'black',
+  ];
+
+  // Fixes Handcrafted Cupboards and Functional Storage 1x1 Drawers conflicting
+  Ingredient.of('#handcrafted:cupboards').itemIds.forEach((item) => {
+    e.remove({ output: item });
+    let item2 = item.split(':')[1].replace('_cupboard', '');
+    e.shaped(`3x ${item}`, ['AAA', 'BCB', 'AAA'], {
+      A: `minecraft:${item2}_planks`,
+      B: `minecraft:${item2}_slab`,
+      C: '#c:chests',
+    }).id(`craftoria:handcrafted/${item2}_cupboard`);
+  });
+
+  // Fixes Handcrafted Wool Sheets conflicting with Comforts Sleeping Bags
+  colors.forEach((wool) => {
+    e.remove({ type: 'minecraft:crafting_shaped', output: `handcrafted:${wool}_sheet` });
+
+    // Sheets
+    e.shaped(`handcrafted:${wool}_sheet`, ['AAA'], {
+      A: `minecraft:${wool}_wool`,
+    }).id(`handcrafted:${wool}_sheet`);
+  });
 });
--- kubejs/server_scripts/Mods/LaserIO/Recipes.js
+++
@@ -1,3 +0,0 @@
-ServerEvents.recipes(e => {
-  e.custom({ type: "laserio:cardclear", category: "misc", group: "laserio", ingredients: [{ item: "laserio:card_chemical" }], result: { count: 1, id: "laserio:card_chemical", } }).id("craftoria:laserio/card_chemical_nbtclear");
-});
