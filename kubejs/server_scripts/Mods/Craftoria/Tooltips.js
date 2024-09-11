a85772df307e52831f3f9ea634e4945ac794526d
WhitePhant0m <kallunki.aleksi@gmail.com>
2024-09-11 20:18:14 +0300

    Remove chunk loaders by default, use FTB Chunks for chunk loading

--- kubejs/server_scripts/Mods/Craftoria/Tooltips.js
+++ kubejs/server_scripts/Mods/Craftoria/Tooltips.js
@@ -1,5 +1,14 @@
 // Tooltips for items/blocks added by Craftoria
 // or things that would've had too little stuff to warrant their own file/folder.
 ItemEvents.modifyTooltips((e) => {
-	e.add('pipe_connector:pipe_connector', { shift: true }, [Text.gold('Check EMI for supported pipes.')]);
+  e.add('pipe_connector:pipe_connector', { shift: true }, [Text.gold('Check EMI for supported pipes.')]);
+
+  globalItemRemovals.forEach((item) => {
+    e.add(item, [
+      Text.red('This item has been disabled.'),
+      Text.red("If you're playing in singleplayer, or are a server admin,"),
+      Text.red("if you want to enable it, go to 'kubejs/removals.js'"),
+      Text.red('and remove the item from globalItemRemovals.'),
+    ]);
+  });
 });
--- kubejs/server_scripts/removals.js
+++ kubejs/server_scripts/removals.js
@@ -7,6 +7,10 @@ const globalItemRemovals = [
   'bigger_ae2:quantum_item_storage_cell',
   'bigger_ae2:digital_singularity_item_storage_cell',
   'chisel:chisel',
+  'ae2:spatial_anchor',
+  'mekanism:upgrade_anchor',
+  'mekanism:dimensional_stabilizer',
+  'pneumaticcraft:chunkloader_upgrade',
 ];
 
 ServerEvents.recipes((event) => {
@@ -18,8 +22,14 @@ ServerEvents.recipes((event) => {
     'mffs:steel_ingot',
   ];
 
+  const output = [];
+
   id.forEach((id) => {
     event.remove({ id: id });
+  });
+
+  output.forEach((output) => {
+    event.remove({ output: output });
   });
 
   globalItemRemovals.forEach((output) => {
