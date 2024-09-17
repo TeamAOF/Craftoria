de76128aa73231dac9ba4a777137a0c290a14fd6
WhitePhant0m <kallunki.aleksi@gmail.com>
2024-09-17 19:26:30 +0300

    Updated HNN mimic prediction to have all Artifacts( -disabled ones)

--- kubejs/server_scripts/Mods/Apothic/Spawners.js
+++ kubejs/server_scripts/Mods/Apothic/Spawners.js
@@ -1,4 +1,4 @@
-ServerEvents.recipes(e => {
+ServerEvents.recipes((e) => {
   /**
    * @author WhitePhantom
    * @description Apothic Spawners modifier recipe
@@ -9,43 +9,34 @@ ServerEvents.recipes(e => {
    */
   let modifier = (item, main, inverse, type) => {
     let recipe = {
-      type: "apothic_spawners:spawner_modifier",
+      type: 'apothic_spawners:spawner_modifier',
       mainhand: {},
-      stat_changes: []
+      stat_changes: [],
     };
     let recipeInverse = {
-      type: "apothic_spawners:spawner_modifier",
+      type: 'apothic_spawners:spawner_modifier',
       mainhand: {},
-      offhand: { tag: "c:gems/quartz" },
-      stat_changes: []
+      offhand: Ingredient.of('minecraft:quartz').toJson(),
+      stat_changes: [],
     };
-    if (item.includes("#")) {
-      recipe.mainhand = { tag: item.replace("#", "") };
-      recipeInverse.mainhand = { tag: item.replace("#", "") };
-    }
-    else {
-      recipe.mainhand = { item: item };
-      recipeInverse.mainhand = { item: item };
-    }
+
+    recipe.mainhand = Ingredient.of(item).toJson();
+    recipeInverse.mainhand = Ingredient.of(item).toJson();
 
     recipe.stat_changes.push({
       type: `apothic_spawners:${type}`,
-      value: main.value
+      value: main.value,
     });
     if (main.limit)
-      if (main.limit.includes("min:"))
-        recipe.stat_changes[0].min = parseInt(main.limit.split(":")[1]);
-      else
-        recipe.stat_changes[0].max = parseInt(main.limit.split(":")[1]);
+      if (main.limit.includes('min:')) recipe.stat_changes[0].min = parseInt(main.limit.split(':')[1]);
+      else recipe.stat_changes[0].max = parseInt(main.limit.split(':')[1]);
     recipeInverse.stat_changes.push({
       type: `apothic_spawners:${type}`,
-      value: inverse.value
+      value: inverse.value,
     });
     if (inverse.limit)
-      if (inverse.limit.includes("min:"))
-        recipeInverse.stat_changes[0].min = parseInt(inverse.limit.split(":")[1]);
-      else
-        recipeInverse.stat_changes[0].max = parseInt(inverse.limit.split(":")[1]);
+      if (inverse.limit.includes('min:')) recipeInverse.stat_changes[0].min = parseInt(inverse.limit.split(':')[1]);
+      else recipeInverse.stat_changes[0].max = parseInt(inverse.limit.split(':')[1]);
 
     e.remove({ id: `apothic_spawners:spawner_modifiers/${type}` });
     e.remove({ id: `apothic_spawners:spawner_modifiers/_inverse/${type}` });
@@ -57,7 +48,7 @@ ServerEvents.recipes(e => {
   // modifier("#c:ingots/iron", { value: 10, limit: "min:10" }, { value: -10, limit: "max:10" }, "min_delay");
   // modifier("#c:gems/diamond", { value: 10, limit: "min:10" }, { value: -10, limit: "max:10" }, "max_delay");
 
-  modifier("minecraft:sugar", { value: -10, limit: "min:100" }, { value: 10, limit: "max:1600" }, "min_delay");
-  modifier("minecraft:clock", { value: -10, limit: "min:100" }, { value: 10, limit: "max:1600" }, "max_delay");
-  modifier("minecraft:fermented_spider_eye", { value: 1, limit: "max:8" }, { value: -1, limit: "min:1" }, "spawn_count");
+  modifier('minecraft:sugar', { value: -10, limit: 'min:100' }, { value: 10, limit: 'max:1600' }, 'min_delay');
+  modifier('minecraft:clock', { value: -10, limit: 'min:100' }, { value: 10, limit: 'max:1600' }, 'max_delay');
+  modifier('minecraft:fermented_spider_eye', { value: 1, limit: 'max:8' }, { value: -1, limit: 'min:1' }, 'spawn_count');
 });
