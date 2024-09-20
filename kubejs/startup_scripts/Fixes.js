c9f7efc534009c9188b541063442e7fecd942e13
WhitePhant0m <kallunki.aleksi@gmail.com>
2024-09-21 01:33:36 +0300

    Made crafting quantum gear take longer, fixed quantum armor stacking
    Also made Quantum gear epic :)

--- kubejs/startup_scripts/Fixes.js
+++ kubejs/startup_scripts/Fixes.js
@@ -1,7 +1,15 @@
 ItemEvents.modification((e) => {
+  // prettier-ignore
   ['cooking_pot', 'food_press', 'frying_pan', 'knife', 'mortar_and_pestle'].forEach((cItem) => {
     e.modify(`croptopia:${cItem}`, (item) => {
       item.craftingRemainder = Item.of(`croptopia:${cItem}`).item;
+    });
+  });
+
+  ['helmet', 'chestplate', 'leggings', 'boots', 'sword'].forEach((qItem) => {
+    e.modify(`modern_industrialization:quantum_${qItem}`, (item) => {
+      item.maxStackSize = 1;
+      item.rarity = 'epic';
     });
   });
 });
