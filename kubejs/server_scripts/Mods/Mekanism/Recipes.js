baadaafc5dfe963ff753198abc6e461c550e73c9
Phantom <33400200+WhitePhant0m@users.noreply.github.com>
2024-09-25 08:18:48 +0300

    "Better" Uranium hexafluoride recipe now uses 399 hydrofluoric acid
    Now the chem amounts make sense.

--- kubejs/server_scripts/Mods/Mekanism/Recipes.js
+++ kubejs/server_scripts/Mods/Mekanism/Recipes.js
@@ -115,7 +115,7 @@ ServerEvents.recipes((e) => {
   // New Stuff
   rotary('mekanism:antimatter', 'craftoria:antimatter');
   oxidizing('200x craftoria:plutonium_oxide', '#c:ingots/plutonium');
-  chem_infuser('400x mekanism:uranium_hexafluoride', '200x mekanism:hydrofluoric_acid', '1x craftoria:plutonium_oxide');
+  chem_infuser('400x mekanism:uranium_hexafluoride', '399x mekanism:hydrofluoric_acid', '1x craftoria:plutonium_oxide');
 
   // Shaped
   e.shaped('mekanism:cardboard_box', [
