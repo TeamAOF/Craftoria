537d53300579c773a40d269db1793592f7cc0010
WhitePhant0m <kallunki.aleksi@gmail.com>
2024-09-25 14:25:46 +0300

    Hide flight potions

--- kubejs/server_scripts/EMIThings/Hide.js
+++ kubejs/server_scripts/EMIThings/Hide.js
@@ -14,4 +14,13 @@ RecipeViewerEvents.removeEntriesCompletely('item', (event) => {
   for (let i = 1; i <= 5; i++) {
     event.remove(Ingredient.of(`minecraft:enchanted_book[stored_enchantments={levels:{"apothic_enchanting:boon_of_the_earth":${i}}}]`));
   }
+
+  ['apothic_attributes:extra_long_flying', 'apothic_attributes:long_flying', 'apothic_attributes:flying'].forEach((potion) => {
+    event.remove([
+      Ingredient.of(`minecraft:potion[potion_contents={potion:"${potion}"}]`),
+      Ingredient.of(`minecraft:splash_potion[potion_contents={potion:"${potion}"}]`),
+      Ingredient.of(`minecraft:lingering_potion[potion_contents={potion:"${potion}"}]`),
+      Ingredient.of(`minecraft:tipped_arrow[potion_contents={potion:"${potion}"}]`),
+    ]);
+  });
 });
