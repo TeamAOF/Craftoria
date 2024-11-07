14233c2567fa8f2b23cbeec7ed9cac356e50916b
AK <anthonkrisat@hotmail.com>
2024-11-07 17:29:29 +0100

    added stone type conversion recipes through AA

---
+++ kubejs/server_scripts/Mods/ActuallyAdditions/Reconstructor.js
@@ -0,0 +1,8 @@
+ServerEvents.recipes(event => {
+
+  event.recipes.actuallyadditions.laser("minecraft:andesite", "minecraft:stone")
+  event.recipes.actuallyadditions.laser("minecraft:granite", "minecraft:andesite")
+  event.recipes.actuallyadditions.laser("minecraft:diorite", "minecraft:granite")
+  event.recipes.actuallyadditions.laser("minecraft:calcite", "minecraft:diorite")
+  event.recipes.actuallyadditions.laser("minecraft:stone", "minecraft:calcite")
+})
