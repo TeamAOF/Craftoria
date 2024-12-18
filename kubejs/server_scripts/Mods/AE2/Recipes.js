6acd8f9b3f221f0c7f6ba2a7caa0a71d6fa47b60
AK <anthonkrisat@hotmail.com>
2024-12-18 23:09:06 +0100

    ae quantum armor now requires mekasuit

--- kubejs/server_scripts/Mods/AE2/Recipes.js
+++ kubejs/server_scripts/Mods/AE2/Recipes.js
@@ -39,4 +39,28 @@ ServerEvents.recipes((event) => {
       M: 'appflux:core_256m',
       R: 'appflux:harden_insulating_resin',
     }).id('bigger_ae2:advanced_flux_cell_housing');
+
+    event.replaceInput(
+      { id: 'advanced_ae:quantum_helmet' },
+      'minecraft:netherite_helmet',
+      'mekanism:mekasuit_helmet'
+    )
+
+    event.replaceInput(
+      { id: 'advanced_ae:quantum_chest' },
+      'minecraft:netherite_chestplate',
+      'mekanism:mekasuit_bodyarmor'
+    )
+
+    event.replaceInput(
+      { id: 'advanced_ae:quantum_leggings' },
+      'minecraft:netherite_leggings',
+      'mekanism:mekasuit_pants'
+    )
+
+    event.replaceInput(
+      { id: 'advanced_ae:quantum_boots' },
+      'minecraft:netherite_boots',
+      'mekanism:mekasuit_boots'
+    )
 });
