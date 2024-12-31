5798f18c88c2dd36c4e77b1b48b89a1fb4b8aba0
AK <anthonkrisat@hotmail.com>
2024-12-31 11:35:00 +0100

    add recipe for Cosmic Injector

---
+++ kubejs/server_scripts/Mods/ModernIndustrialization/Assembler.js
@@ -0,0 +1,11 @@
+ServerEvents.recipes((e) => {
+  let assembler = e.recipes.modern_industrialization.assembler;
+
+  assembler(10000000, 1000)
+    .itemOut('craftoria:cosmic_injector')
+    .itemIn('3x craftoria:cosmic_matter')
+    .itemIn('2x modern_industrialization:stainless_steel_bolt')
+    .itemIn('2x minecraft:glass_pane')
+    .itemIn('4x modern_industrialization:bronze_plate')
+    .id('craftoria:assembler/electric_age/cosmic_injector');
+});
