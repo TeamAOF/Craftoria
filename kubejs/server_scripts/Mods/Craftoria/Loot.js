3ac776629c796f6eede01809888368518eb95117
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-01-03 19:00:53 +0200

    Initial BOMD gate

--- kubejs/server_scripts/Mods/Craftoria/Loot.js
+++ kubejs/server_scripts/Mods/Craftoria/Loot.js
@@ -5,6 +5,13 @@ LootJS.lootTables((e) => {
   e.forEachTable((table) => {
     table.removeItem(removeLoot);
   });
+
+  e.create('bosses_of_mass_destruction:entities/obsidilith', 'entity').createPool((pool) => {
+    pool.addEntry(LootEntry.of('bosses_of_mass_destruction:obsidian_heart').setCount(2));
+  });
+  e.create('bosses_of_mass_destruction:entities/gauntlet', 'entity').createPool((pool) => {
+    pool.addEntry(LootEntry.of('bosses_of_mass_destruction:blazing_eye').setCount(2));
+  });
 });
 
 LootJS.modifiers((e) => {
