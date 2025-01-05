d7f08bebca616f9cdcee3a8c844f11482f6dd7b7
AK <anthonkrisat@hotmail.com>
2025-01-05 13:17:04 +0100

    re-enable lich projectiles as crash is fixed

--- kubejs/server_scripts/Mods/Craftoria/Entities.js
+++ kubejs/server_scripts/Mods/Craftoria/Entities.js
@@ -1,3 +1,5 @@
+/*
 EntityEvents.spawned(['bosses_of_mass_destruction:blue_fireball', 'bosses_of_mass_destruction:comet'], (e) => {
   e.cancel(); // Have to disable the liches projectiles, they crash on spawn/impact. TODO: Remove this when fixed in the mod.
 });
+*/
