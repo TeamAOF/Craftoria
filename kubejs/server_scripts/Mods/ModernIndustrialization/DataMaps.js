c8a0f7369041aa5da20954ab1f596cf5555ed4af
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-01-06 16:00:24 +0200

    I don't like pure json files

---
+++ kubejs/server_scripts/Mods/ModernIndustrialization/DataMaps.js
@@ -0,0 +1,27 @@
+ServerEvents.generateData('after_mods', (e) => {
+  e.json('extended_industrialization:data_maps/block/large_electric_furnace_tier', {
+    values: {
+      'modern_industrialization:superconductor_coil': {
+        batch_size: 256,
+        eu_cost_multiplier: 0.25,
+      },
+    },
+  });
+
+  e.json('mi_tweaks:data_maps/item/water_explosive', {
+    values: {
+      'modern_industrialization:sodium_dust': {
+        strength: 3,
+        fire: false,
+      },
+      'modern_industrialization:sodium_block': {
+        strength: 9,
+        fire: false,
+      },
+      'modern_industrialization:sodium_tiny_dust': {
+        strength: 1,
+        fire: false,
+      },
+    },
+  });
+});
