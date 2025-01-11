76a22c32a696ae3c5f34721ce4249ef0c8e411b7
Inno <132615999+Kazuhiko-Gushiken@users.noreply.github.com>
2025-01-11 01:01:20 -0800

    Update DataMaps.js
    nerf milk

--- kubejs/server_scripts/Mods/Powah/DataMaps.js
+++ kubejs/server_scripts/Mods/Powah/DataMaps.js
@@ -2,19 +2,19 @@ ServerEvents.generateData('after_mods', (e) => {
   e.json('powah:data_maps/fluid/fluid_coolant', {
     values: {
       '#c:water': {
-        temperature: 1,
+        temperature: 0,
       },
       'industrialforegoing:ether_gas': {
-        temperature: -69,
+        temperature: -116,
       },
       'modern_industrialization:cryofluid': {
-        temperature: -250,
+        temperature: -273,
       },
       'modern_industrialization:liquid_air': {
-        temperature: -183,
+        temperature: -196,
       },
       'minecraft:milk': {
-        temperature: 5,
+        temperature: 2,
       },
     },
   });
