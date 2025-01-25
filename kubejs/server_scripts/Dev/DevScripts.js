26633dc92bc6a9bf2f317873d8c2f79136d3f052
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-01-25 19:48:19 +0200

    Don't depend on MI debug commands for building the multiblocks

--- kubejs/server_scripts/Dev/DevScripts.js
+++ kubejs/server_scripts/Dev/DevScripts.js
@@ -3,13 +3,20 @@ if (global.devEnv) {
 
   // This script allows you to build MI multiblocks by right-clicking with a stick in your main hand and a wrench in your off-hand.
   ItemEvents.firstRightClicked('minecraft:stick', (event) => {
-    const { player, target } = event;
+    const { player, target, level } = event;
     if (player.offHandItem.item.id !== 'modern_industrialization:wrench') return;
 
     if (target.block.entity instanceof $MultiblockMachineBlockEntity) {
+      /** @type $MultiblockMachineBlockEntity */
+      const multiblock = target.block.entity;
       const { x, y, z } = target.block.pos;
+      // const shape = entity.getActiveShape();
+      const shapeMatcher = entity.createShapeMatcher();
+      const updatedBlocks = shapeMatcher.buildMultiblock(level);
 
-      player.runCommand(`/mi machines build_multiblock ${x} ${y} ${z}`);
+      player.tell(`Built multiblock at ${x}, ${y}, ${z}`);
+      player.tell(`Updated ${updatedBlocks} blocks`);
+      // player.tell(`Shape: ${shape}`);
     }
   });
 }
