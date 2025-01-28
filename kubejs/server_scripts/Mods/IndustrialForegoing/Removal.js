b47de2b3728b6216f1176d4486d0153c61f9846f
Inno <132615999+Kazuhiko-Gushiken@users.noreply.github.com>
2025-01-28 14:49:43 -0800

    remove infinity stuff recipes

---
+++ kubejs/server_scripts/Mods/IndustrialForegoing/Removal.js
@@ -0,0 +1,12 @@
+ServerEvents.recipes(event => {
+
+
+    event.remove({output: 'industrialforegoing:infinity_nuke'})
+    event.remove({output: 'industrialforegoing:infinity_launcher'})
+    event.remove({output: 'industrialforegoing:infinity_backpack'})
+    event.remove({output: 'industrialforegoing:infinity_trident'})
+    event.remove({output: 'industrialforegoing:infinity_hammer'})
+    event.remove({output: 'industrialforegoing:infinity_saw'})
+    event.remove({output: 'industrialforegoing:infinity_drill'})
+}
+)
