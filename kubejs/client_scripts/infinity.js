2c904f89ef4ee8707f867a739ce27feef3d403b4
Inno <132615999+Kazuhiko-Gushiken@users.noreply.github.com>
2025-01-28 14:52:50 -0800

    Add files via upload

---
+++ kubejs/client_scripts/infinity.js
@@ -0,0 +1,10 @@
+ItemEvents.modifyTooltips(event => {
+
+    event.add('industrialforegoing:infinity_nuke', ['§cDisabled.', '§cTo Re-enable, delete the infinity.js file in kubejs/client_scripts and kubejs/server_scripts.'])
+    event.add('industrialforegoing:infinity_launcher', ['§cDisabled.', '§cTo Re-enable, delete the infinity.js file in kubejs/client_scripts and kubejs/server_scripts.'])
+    event.add('industrialforegoing:infinity_backpack', ['§cDisabled.', '§cTo Re-enable, delete the infinity.js file in kubejs/client_scripts and kubejs/server_scripts.'])
+    event.add('industrialforegoing:infinity_trident', ['§cDisabled.', '§cTo Re-enable, delete the infinity.js file in kubejs/client_scripts and kubejs/server_scripts.'])
+    event.add('industrialforegoing:infinity_hammer', ['§cDisabled.', '§cTo Re-enable, delete the infinity.js file in kubejs/client_scripts and kubejs/server_scripts.'])
+    event.add('industrialforegoing:infinity_saw', ['§cDisabled.', '§cTo Re-enable, delete the infinity.js file in kubejs/client_scripts and kubejs/server_scripts.'])
+    event.add('industrialforegoing:infinity_drill', ['§cDisabled.', '§cTo Re-enable, delete the infinity.js file in kubejs/client_scripts and kubejs/server_scripts.'])
+  })
