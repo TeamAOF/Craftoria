6602ae85da6f8559be0f13577901f3f170560cc8
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-02-19 23:16:40 +0200

    #421 but arrays
    closes #421

--- kubejs/server_scripts/unify/Knife.js
+++ kubejs/server_scripts/unify/Knife.js
@@ -1,23 +1,28 @@
-ServerEvents.recipes((e) => {
-  Ingredient.of('#c:tools/knife').stacks.forEach((item) => {
-    if (item.mod === 'farmersdelight' || item.mod === 'moredelight' || item.id === 'aquaculture:neptunium_fillet_knife') return;
-    globalItemRemovals.push(item.id);
+{
+  let modWhitelist = ['farmersdelight', 'moredelight', 'arsdelight', 'twilightdelight', 'ends_delight'];
+  let itemWhitelist = ['aquaculture:neptunium_fillet_knife'];
+
+  ServerEvents.recipes((e) => {
+    Ingredient.of('#c:tools/knife').stacks.forEach((item) => {
+      if (modWhitelist.includes(item.mod) || itemWhitelist.includes(item.id)) return;
+      globalItemRemovals.push(item.id);
+    });
+
+    e.shaped('aquaculture:neptunium_fillet_knife', ['N', 'S'], {
+      N: 'aquaculture:neptunium_ingot',
+      S: 'minecraft:stick',
+    }).id('aquaculture:neptunium_fillet_knife');
   });
 
-  e.shaped('aquaculture:neptunium_fillet_knife', ['N', 'S'], {
-    N: 'aquaculture:neptunium_ingot',
-    S: 'minecraft:stick',
-  }).id('aquaculture:neptunium_fillet_knife');
-});
+  ServerEvents.tags('item', (e) => {
+    e.add('c:tools/knife', Ingredient.of('#c:tools/knives').itemIds);
+    e.add('c:tools/knives', Ingredient.of('#c:tools/knife').itemIds);
 
-ServerEvents.tags('item', (e) => {
-  e.add('c:tools/knife', Ingredient.of('#c:tools/knives').itemIds);
-  e.add('c:tools/knives', Ingredient.of('#c:tools/knife').itemIds);
-
-  let hiddenKnives = [];
-  Ingredient.of('#c:tools/knife').stacks.forEach((item) => {
-    if (item.mod === 'farmersdelight' || item.mod === 'moredelight' || item.id === 'aquaculture:neptunium_fillet_knife') return;
-    hiddenKnives.push(item.id);
+    let hiddenKnives = [];
+    Ingredient.of('#c:tools/knife').stacks.forEach((item) => {
+      if (modWhitelist.includes(item.mod) || itemWhitelist.includes(item.id)) return;
+      hiddenKnives.push(item.id);
+    });
+    e.add('almostunified:hide', hiddenKnives);
   });
-  e.add('almostunified:hide', hiddenKnives);
-});
+}
