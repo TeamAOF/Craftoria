afd4ec88164e3e3a4fa540ff214857f99d1a0bed
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-02-28 09:02:47 +0200

    XP Fluid cell

--- kubejs/startup_scripts/Custom/InfinityCells.js
+++ kubejs/startup_scripts/Custom/InfinityCells.js
@@ -3,16 +3,16 @@ const $Chem = Java.loadClass('mekanism.common.registries.MekanismChemicals');
 const $SoulKey = Java.loadClass('com.buuz135.soulplied_energistics.applied.SoulKey');
 const $FluxKey = Java.loadClass('com.glodblock.github.appflux.common.me.key.FluxKey');
 const $EnergyType = Java.loadClass('com.glodblock.github.appflux.common.me.key.type.EnergyType');
-// const $ExperienceKey = Java.loadClass('es.degrassi.appexp.me.key.ExperienceKey');
 const $SourceKey = Java.loadClass('gripe._90.arseng.me.key.SourceKey');
 
 /**
  * Add Infinity Cells.
- * Available types are 'item', 'fluid', 'chemical', 'soul', and 'flux'.
+ * Available types are 'item', 'fluid', 'chemical', 'soul', 'flux', 'source'.
  * 'item' and 'fluid' are pretty self-explanatory.
  * 'chemical' is for Mekanism chemicals, which don't need a mod ID.
  * 'soul' is for Soulplied Energistics. It doesn't need an ID. Just use 'soul'.
  * 'flux' is for AppliedFlux. It doesn't need an ID. Just use 'flux'.
+ * 'source' is for Arseng. It doesn't need an ID. Just use 'source'.
  */
 global.infCells = [
   // I don't have textures for these just yet, so I'm commenting them out for now. Also not sure which ones to actually include.
@@ -20,18 +20,18 @@ global.infCells = [
   // { id: 'minecraft:gravel', type: 'item' },
   { id: 'minecraft:lava', type: 'fluid' },
   // { id: 'minecraft:milk', type: 'fluid' },
-  // { id: 'justdirethings:xp_fluid_source', type: 'fluid' },
+  { id: 'justdirethings:xp_fluid_source', type: 'fluid' },
   { id: 'modern_industrialization:uu_matter', type: 'fluid' },
   // { id: 'nuclear_waste', type: 'chemical' },
   { id: 'soul', type: 'soul' },
   { id: 'flux', type: 'flux' },
-  // { id: 'experience', type: 'experience' },
   { id: 'source', type: 'source' },
 ];
 
 StartupEvents.registry('item', (e) => {
   let createInfCell = (id, type) => {
     let strippedId = id.includes(':') ? id.split(':')[1] : id;
+    if (type === 'fluid' && strippedId.includes('_source')) strippedId.replace('_source', '');
 
     let cell = e
       .create(`craftoria:infinity_${strippedId}_cell`, 'custom_infinity_cell')
@@ -54,9 +54,6 @@ StartupEvents.registry('item', (e) => {
       case 'flux':
         cell.type(() => $FluxKey.of($EnergyType.FE));
         break;
-      // case 'experience':
-      //   cell.type(() => $ExperienceKey.KEY);
-      //   break;
       case 'source':
         cell.type(() => $SourceKey.KEY);
         break;
