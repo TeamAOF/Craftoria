b4c873da095eb0bc676c4adfed665b775ff8ffbf
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-03-15 01:05:48 +0200

    apoth change stuff >:(

---
+++ kubejs/server_scripts/Mods/Apothic/LootCategoryOverrides.js
@@ -0,0 +1,12 @@
+ServerEvents.generateData('after_mods', (e) => {
+  e.json('apotheosis:data_maps/item/loot_category_overrides', {
+    values: {
+      'mekaweapons:meka_tana': 'apotheosis:melee_weapon',
+      'mekaweapons:meka_bow': 'apotheosis:bow',
+      'industrialforegoing:infinity_drill': 'apotheosis:breaker',
+      'industrialforegoing:infinity_saw': 'apotheosis:breaker',
+      'industrialforegoing:infinity_hammer': 'apotheosis:melee_weapon',
+      'industrialforegoing:infinity_trident': 'apotheosis:trident',
+    },
+  });
+});
