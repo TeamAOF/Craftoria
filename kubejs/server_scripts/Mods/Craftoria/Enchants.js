5530dc177009f40236a7788a1e9f773add03d15e
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-03-15 04:29:28 +0200

    also disable the recipe, otherwise whole ars EMI plugin explodes .-.

--- kubejs/server_scripts/Mods/Craftoria/Enchants.js
+++ kubejs/server_scripts/Mods/Craftoria/Enchants.js
@@ -1,5 +1,5 @@
 ServerEvents.generateData('after_mods', (e) => {
-  const enchantsToDisable = ['apothic_enchanting:enchantment/boon_of_the_earth', 'ars_elemental:enchantment/soulbound'];
+  const enchantsToDisable = ['apothic_enchanting:enchantment/boon_of_the_earth', 'ars_elemental:enchantment/soulbound', 'ars_elemental:recipe/soulbound_1'];
 
   enchantsToDisable.forEach((enchant) => {
     e.json(enchant, {
