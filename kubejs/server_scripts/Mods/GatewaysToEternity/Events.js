0110c64bd28cc3282a68657e0291f1ec6aa9c2c4
Aryxst <jessieodeh08@gmail.com>
2025-03-23 22:02:20 +0100

    fix: listen for right click events for any block

--- kubejs/server_scripts/Mods/GatewaysToEternity/Events.js
+++ kubejs/server_scripts/Mods/GatewaysToEternity/Events.js
@@ -1,5 +1,6 @@
-BlockEvents.rightClicked("compactmachines:solid_wall", (e) => {
-  if (e.item.id == "gateways:gate_pearl") {
+BlockEvents.rightClicked((e) => {
+  if (e.item.id != "gateways:gate_pearl") return;
+  if (e.level.dimension == "compactmachines:compact_world") {
     e.player.tell("Gates cannot be opened in this dimension.");
     e.cancel();
   }
