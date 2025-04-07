49b9cd20d1f73bf78a1aff52463a16953da24ae0
Aryxst <jessieodeh08@gmail.com>
2025-04-07 19:36:50 +0200

    refactor!: move Mekanism recipe creation methods to helper function
    Consolidates Mekanism recipe creation logic into a dedicated helper function.
    This improves code organization and reusability across scripts.
    The `MekanismHelper` in `mekanism_helper.js` now encapsulates recipe creation methods.
    Updates `Recipes.js` and `mekProcessing.js` to utilize this helper, resulting in cleaner and more maintainable recipe methods.

--- kubejs/server_scripts/Mods/Mekanism/Recipes.js
+++ kubejs/server_scripts/Mods/Mekanism/Recipes.js
@@ -1,143 +1,32 @@
 ServerEvents.recipes((e) => {
-  let makeID = (type, output, input) => {
-    return _makeID('mekanism', type, output, input);
-  };
-
-  let enrich = (output, input) => {
-    e.recipes.mekanism.enriching(output, input).id(makeID('enriching', output, input));
-  };
-
-  let metallurgic_infusing = (output, input, chem, perTick) => {
-    e.recipes.mekanism.metallurgic_infusing(output, input, chem, perTick ? perTick : false).id(makeID('metallurgic_infusing', output, input));
-  };
-
-  let crush = (output, input) => {
-    e.recipes.mekanism.crushing(output, input).id(makeID('crushing', output, input));
-  };
-
-  let injecting = (output, input, chem) => {
-    e.recipes.mekanism.injecting(output, input, chem, false).id(makeID('injecting', output, input));
-  };
-
-  let crystallizing = (output, input) => {
-    e.recipes.mekanism.crystallizing(output, input).id(makeID('crystallizing', output, input));
-  };
-
-  let chem_infuser = (output, chemLeft, chemRight) => {
-    e.recipes.mekanism.chemical_infusing(output, chemLeft, chemRight).id(makeID('chemical_infusing', output, chemLeft));
-  };
-
-  let reaction = (item_out, item_in, fluid_in, chem_out, chem_in, duration) => {
-    let recipe = {
-      type: 'mekanism:reaction',
-      item_input: {
-        type: 'neoforge:compound',
-        children: [],
-        count: item_in[1],
-      },
-      item_output: Item.of(item_out).toJson(),
-      fluid_input: {},
-      chemical_input: {},
-      chemical_output: {},
-      duration: duration,
-    };
-
-    item_in[0].forEach((item) => {
-      recipe.item_input.children.push(Ingredient.of(item).toJson());
-    });
-
-    if (fluid_in.split(' ')[1].includes('#')) recipe.fluid_input.tag = fluid_in.split(' ')[1].replace('#', '');
-    else recipe.fluid_input.id = fluid_in.split(' ')[1];
-    recipe.fluid_input.amount = parseInt(fluid_in.split(' ')[0].replace('x', ''));
-
-    if (chem_in.split(' ')[1].includes('#')) recipe.chemical_input.chemical = chem_in.split(' ')[1].replace('#', '');
-    else recipe.chemical_input.id = chem_in.split(' ')[1];
-    recipe.chemical_input.amount = parseInt(chem_in.split(' ')[0].replace('x', ''));
-
-    recipe.chemical_output.id = chem_out.split(' ')[1];
-    recipe.chemical_output.amount = parseInt(chem_out.split(' ')[0].replace('x', ''));
-
-    e.custom(recipe).id(makeID('reaction', item_out, item_in[0][0]));
-  };
-
-  let rotary = (chem, fluid) => {
-    let recipe = {
-      type: 'mekanism:rotary',
-      chemical_input: {
-        amount: 1,
-        chemical: chem,
-      },
-      chemical_output: {
-        amount: 1,
-        id: chem,
-      },
-      fluid_input: {
-        amount: 1,
-        tag: `c:${fluid.split(':')[1]}`,
-      },
-      fluid_output: {
-        amount: 1,
-        id: fluid,
-      },
-    };
-
-    e.custom(recipe).id(makeID('rotary', chem, fluid));
-  };
-
-  let nucleosynthesizing = (output, input, chem, duration) => {
-    let recipe = {
-      type: 'mekanism:nucleosynthesizing',
-      chemical_input: {
-        amount: parseInt(chem.split('x ')[0]) || 1000,
-        chemical: chem.split('x ')[1] || chem,
-      },
-      duration: duration || 100,
-      item_input: Ingredient.of(input).toJson(),
-      output: Item.of(output).toJson(),
-      per_tick_usage: false,
-    };
-
-    e.custom(recipe).id(makeID('nucleosynthesizing', output, input));
-  };
-
-  let oxidizing = (output, input) => {
-    let recipe = {
-      type: 'mekanism:oxidizing',
-      input: Ingredient.of(input).toJson(),
-      output: {
-        amount: parseInt(output.split('x ')[0]),
-        id: output.split('x ')[1],
-      },
-    };
-    e.custom(recipe).id(makeID('oxidizing', output, input));
-  };
+  const mekanism = MekanismHelper(e);
 
   // QoL Recipes
-  metallurgic_infusing('4x mekanism:dust_refined_obsidian', 'minecraft:obsidian', '40x mekanism:diamond');
-  metallurgic_infusing('9x mekanism:alloy_infused', '#c:storage_blocks/copper', '90x mekanism:redstone');
-  metallurgic_infusing('9x mekanism:basic_control_circuit', '#c:storage_blocks/osmium', '180x mekanism:redstone');
+  mekanism.metallurgicInfusing('4x mekanism:dust_refined_obsidian', 'minecraft:obsidian', '40x mekanism:diamond');
+  mekanism.metallurgicInfusing('9x mekanism:alloy_infused', '#c:storage_blocks/copper', '90x mekanism:redstone');
+  mekanism.metallurgicInfusing('9x mekanism:basic_control_circuit', '#c:storage_blocks/osmium', '180x mekanism:redstone');
 
   // MI Compat
-  enrich('2x modern_industrialization:lignite_coal', '#c:ores/lignite_coal');
-  crush('modern_industrialization:lignite_coal_dust', '#c:gems/lignite_coal');
-  crush('modern_industrialization:brick_dust', 'minecraft:brick');
-  crush('4x modern_industrialization:brick_dust', 'minecraft:bricks');
-  enrich('6x modern_industrialization:salt_dust', '#c:ores/salt');
+  mekanism.enriching('2x modern_industrialization:lignite_coal', '#c:ores/lignite_coal');
+  mekanism.crushing('modern_industrialization:lignite_coal_dust', '#c:gems/lignite_coal');
+  mekanism.crushing('modern_industrialization:brick_dust', 'minecraft:brick');
+  mekanism.crushing('4x modern_industrialization:brick_dust', 'minecraft:bricks');
+  mekanism.enriching('6x modern_industrialization:salt_dust', '#c:ores/salt');
 
-  enrich('2x modern_industrialization:antimony_dust', '#c:ores/antimony');
-  enrich('4x modern_industrialization:antimony_dust', '3x #c:raw_materials/antimony');
-  enrich('4x modern_industrialization:monazite_dust', '#c:ores/monazite');
-  enrich('3x irons_spellbooks:raw_mithril', '#c:ores/mithril');
+  mekanism.enriching('2x modern_industrialization:antimony_dust', '#c:ores/antimony');
+  mekanism.enriching('4x modern_industrialization:antimony_dust', '3x #c:raw_materials/antimony');
+  mekanism.enriching('4x modern_industrialization:monazite_dust', '#c:ores/monazite');
+  mekanism.enriching('3x irons_spellbooks:raw_mithril', '#c:ores/mithril');
 
   // AA Compat
-  enrich('2x actuallyadditions:black_quartz', '#c:ores/black_quartz');
+  mekanism.enriching('2x actuallyadditions:black_quartz', '#c:ores/black_quartz');
 
   // New Stuff
-  rotary('mekanism:antimatter', 'craftoria:antimatter');
-  oxidizing('200x craftoria:plutonium_oxide', '#c:ingots/plutonium');
-  chem_infuser('400x mekanism:uranium_hexafluoride', '399x mekanism:hydrofluoric_acid', '1x craftoria:plutonium_oxide');
+  mekanism.rotaryCondensentrating('mekanism:antimatter', 'craftoria:antimatter');
+  mekanism.rotaryCondensentrating('craftoria:quantum_infusion', 'advanced_ae:quantum_infusion_source');
+  mekanism.oxidizing('200x craftoria:plutonium_oxide', '#c:ingots/plutonium');
+  mekanism.chemicalInfusing('400x mekanism:uranium_hexafluoride', '399x mekanism:hydrofluoric_acid', '1x craftoria:plutonium_oxide');
 
-  rotary('craftoria:quantum_infusion', 'advanced_ae:quantum_infusion_source');
   // Shaped
 
   // Cardboard Box
@@ -148,7 +37,7 @@ ServerEvents.recipes((e) => {
   }).id('mekanism:cardboard_box');
 
   // Replica
-  nucleosynthesizing('replication:replica_ingot', 'replication:raw_replica', '200x craftoria:quantum_infusion', 250);
+  mekanism.nucleosynthesizing('replication:replica_ingot', 'replication:raw_replica', '200x craftoria:quantum_infusion', 250);
 
   // MekaSuit / Weapons
   e.shaped('mekaweapons:katana_blade', ['  B', 'CB ', 'AC '], {
@@ -164,14 +53,14 @@ ServerEvents.recipes((e) => {
     D: 'mekanism_extras:cosmic_control_circuit',
   }).id('mekaweapons:bow_riser');
 
-  e.shaped('mekanism:jetpack', ['ADA','BCB',' B '], {
+  e.shaped('mekanism:jetpack', ['ADA', 'BCB', ' B '], {
     A: 'modern_industrialization:steel_ingot',
     B: 'modern_industrialization:tin_double_ingot',
     C: 'mekanism:ultimate_chemical_tank',
     D: 'mekanism:ultimate_control_circuit',
   }).id('mekanism:jetpack');
 
-  e.shaped('mekanism:jetpack_armored', ['A A','BCB',' D '], {
+  e.shaped('mekanism:jetpack_armored', ['A A', 'BCB', ' D '], {
     A: 'mekanism:dust_diamond',
     B: 'modern_industrialization:bronze_double_ingot',
     C: 'modern_industrialization:steel_block',
---
+++ kubejs/server_scripts/preload/mekanism_helper.js
@@ -0,0 +1,243 @@
+/**
+ * Mekanism Recipe Helpers
+ * @param {$RecipesKubeEvent_} event
+ */
+function MekanismHelper(event) {
+  const makeRecipeId = (type, output, input) => {
+    return _makeID("mekanism", type, output, input);
+  };
+
+  return {
+    /**
+     * @param {$ItemStack_} output
+     * @param {$ItemStack_} input
+     */
+    enriching(output, input) {
+      event.recipes.mekanism
+        .enriching(output, input)
+        .id(makeRecipeId("enriching", output, input));
+    },
+    /**
+     * @param {$ItemStack_} output
+     * @param {$ItemStack_} input
+     */
+    crushing(output, input) {
+      event.recipes.mekanism
+        .crushing(output, input)
+        .id(makeRecipeId("crushing", output, input));
+    },
+    /**
+     * @param {$ItemStack_} output
+     * @param {$ItemStack_} input
+     * @param {$Chemical_} chemicalInput
+     */
+    metallurgicInfusing(output, input, chemicalInput, perTickUsage) {
+      event.recipes.mekanism
+        .metallurgic_infusing(
+          output,
+          input,
+          chemicalInput,
+          perTickUsage ? perTickUsage : false
+        )
+        .id(makeRecipeId("metallurgic_infusing", output, input));
+    },
+    /**
+     * @param {$ItemStack_} output
+     * @param {$ItemStack_} input
+     * @param {$Chemical_} chemicalInput
+     */
+    injecting(output, input, chemicalInput) {
+      event.recipes.mekanism
+        .injecting(output, input, chemicalInput, false)
+        .id(makeRecipeId("injecting", output, input));
+    },
+    /**
+     * @param {$ItemStack_} output
+     * @param {$ItemStack_} input
+     */
+    crystallizing(output, input) {
+      event.recipes.mekanism
+        .crystallizing(output, input)
+        .id(makeRecipeId("crystallizing", output, input));
+    },
+    /**
+     * @param {$Chemical_} output
+     * @param {$Chemical_} chemicalLeftInput
+     * @param {$Chemical_} chemicalRightInput
+     */
+    chemicalInfusing(output, chemicalLeftInput, chemicalRightInput) {
+      event.recipes.mekanism
+        .chemical_infusing(output, chemicalLeftInput, chemicalRightInput)
+        .id(makeRecipeId("chemical_infusing", output, chemicalLeftInput));
+    },
+    /**
+     * @param {$ItemStack_} itemOutput
+     * @param {[$ItemStack_[], number]} itemInputListWithCount
+     * @param {$Fluid_} fluidInput
+     * @param {$Chemical_} chemicalOutput
+     * @param {$Chemical_} chemicalInput
+     * @param {number} duration
+     */
+    reaction(
+      itemOutput,
+      itemInputListWithCount,
+      fluidInput,
+      chemicalOutput,
+      chemicalInput,
+      duration
+    ) {
+      const recipe = {
+        type: "mekanism:reaction",
+        item_input: {
+          type: "neoforge:compound",
+          children: [],
+          count: itemInputListWithCount[1],
+        },
+        item_output: Item.of(itemOutput).toJson(),
+        fluid_input: {},
+        chemical_input: {},
+        chemical_output: {},
+        duration: duration || 100,
+      };
+
+      itemInputListWithCount[0].forEach((item) => {
+        recipe.item_input.children.push(Ingredient.of(item).toJson());
+      });
+
+      const [fluidAmount, fluidId] = fluidInput.replace("x", "").split(" ");
+      if (fluidId.includes("#")) {
+        recipe.fluid_input.tag = fluidId.replace("#", "");
+      } else {
+        recipe.fluid_input.id = fluidId;
+      }
+      recipe.fluid_input.amount = parseInt(fluidAmount);
+
+      const [chemicalInputAmount, chemicalInputId] = chemicalInput
+        .replace("x", "")
+        .split(" ");
+      if (chemicalInputId.includes("#")) {
+        recipe.chemical_input.chemical = chemicalInputId.replace("#", "");
+      } else {
+        recipe.chemical_input.id = chemicalInputId;
+      }
+      recipe.chemical_input.amount = parseInt(chemicalInputAmount);
+
+      const [chemicalOutputAmount, chemicalOutputId] = chemicalOutput
+        .replace("x", "")
+        .split(" ");
+      recipe.chemical_output.id = chemicalOutputId;
+      recipe.chemical_output.amount = parseInt(chemicalOutputAmount);
+
+      event
+        .custom(recipe)
+        .id(makeRecipeId("reaction", itemOutput, itemInputListWithCount[0][0]));
+    },
+
+    /**
+     * @param {$Chemical_} chemicalInput
+     * @param {$Fluid_} fluidId
+     */
+
+    rotaryCondensentrating(chemicalInput, fluidId) {
+      const recipe = {
+        type: "mekanism:rotary",
+        chemical_input: {
+          amount: 1,
+          chemical: chemicalInput,
+        },
+        chemical_output: {
+          amount: 1,
+          id: chemicalInput,
+        },
+        fluid_input: {
+          amount: 1,
+          tag: `c:${fluidId.split(":")[1]}`,
+        },
+        fluid_output: {
+          amount: 1,
+          id: fluidId,
+        },
+      };
+
+      event.custom(recipe).id(makeRecipeId("rotary", chemicalInput, fluidId));
+    },
+
+    /**
+     * @param {$ItemStack_} itemOutput
+     * @param {$ItemStack_} itemInput
+     * @param {$Chemical_} chemicalInput
+     * @param {number} duration
+     */
+    nucleosynthesizing(itemOutput, itemInput, chemicalInput, duration) {
+      const [chemicalAmount, chemicalId] = chemicalInput.split("x ");
+      const recipe = {
+        type: "mekanism:nucleosynthesizing",
+        chemical_input: {
+          amount: parseInt(chemicalAmount) || 1000,
+          chemical: chemicalId || chemicalInput,
+        },
+        duration: duration || 100,
+        item_input: Ingredient.of(itemInput).toJson(),
+        output: Item.of(itemOutput).toJson(),
+        per_tick_usage: false,
+      };
+
+      event
+        .custom(recipe)
+        .id(makeRecipeId("nucleosynthesizing", itemOutput, itemInput));
+    },
+    /**
+     * @param {$Chemical_} chemicalOutput
+     * @param {$ItemStack_} itemInput
+     * */
+    oxidizing(chemicalOutput, itemInput) {
+      const [chemicalAmount, chemicalId] = chemicalOutput.split("x ");
+      const recipe = {
+        type: "mekanism:oxidizing",
+        input: Ingredient.of(itemInput).toJson(),
+        output: {
+          amount: parseInt(chemicalAmount),
+          id: chemicalId,
+        },
+      };
+
+      event
+        .custom(recipe)
+        .id(makeRecipeId("oxidizing", chemicalOutput, itemInput));
+    },
+    /**
+     * @param {$Item_} itemTagInput
+     * @param {$ItemStack_} mainItemOutput
+     * @param {$ItemStack_} secondaryItemOutput
+     * @param {number} secondaryItemChance
+     * */
+    sawing(
+      itemTagInput,
+      mainItemOutput,
+      secondaryItemOutput,
+      secondaryItemChance
+    ) {
+      const [mainItemAmount, mainItemId] = mainItemOutput.split("x ");
+      const recipe = {
+        type: "mekanism:sawing",
+        input: {
+          count: 1,
+          tag: itemTagInput,
+        },
+        main_output: {
+          count: parseInt(mainItemAmount),
+          id: mainItemId,
+        },
+        secondary_chance: secondaryItemChance,
+        secondary_output: {
+          count: 1,
+          id: secondaryItemOutput,
+        },
+      };
+
+      event
+        .custom(recipe)
+        .id(makeRecipeId("sawing", mainItemOutput, itemTagInput));
+    },
+  };
+}
