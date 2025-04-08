ba468816551964c498d85805dc2f3f7c3d3211a3
Aryxst <jessieodeh08@gmail.com>
2025-04-08 18:28:16 +0200

    refactor: replace global `dyeColors` with `Color.DYE`
    Simplifies dye color handling by using `Color.DYE`

--- kubejs/startup_scripts/Globals.js
+++ kubejs/startup_scripts/Globals.js
@@ -1,9 +1,5 @@
 // priority: 0
 
-const $DyeColor = Java.loadClass('net.minecraft.world.item.DyeColor');
-
-global.dyeColors = $DyeColor.values().map((color) => color.toString().toLowerCase());
-
 /**
  * Enables a few scripts that are only useful for development.
  * Make sure to disable this in production/before pushing to git.
