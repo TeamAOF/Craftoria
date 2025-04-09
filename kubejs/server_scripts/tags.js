24f188c0932d0ce1238b7bb62f669bdfffa1b6b0
Aryxst <jessieodeh08@gmail.com>
2025-04-09 20:02:17 +0200

    feat: add `craftoria:mob_blacklist` tag

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -169,9 +169,11 @@ ServerEvents.tags('fluid', (e) => {
 });
 
 ServerEvents.tags('entity_type', (e) => {
+  e.add("craftoria:mob_blacklist", ['artifacts:mimic', 'minecraft:warden', '#c:bosses', /^occultism:(?!possessed).*$/, '#neoforge:bosses'])
+
   e.add('ftbchunks:entity_interact_whitelist', ['minecraft:villager', 'minecraft:wandering_trader']);
 
-  e.add('mob_grinding_utils:no_swab', ['artifacts:mimic', 'minecraft:warden', '#c:bosses', /^occultism:(?!possessed).*$/, '#neoforge:bosses']);
+  e.add('mob_grinding_utils:no_swab', "#craftoria:mob_blacklist");
 
   e.add('industrialforegoing:mob_crusher_blacklist', ['#c:bosses', '#neoforge:bosses']);
 });
