dea90cc2012be9df5bc1df3a3ead2873aa91952a
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-04-15 15:15:20 +0300

    Add Particle Accelerator multiblock - W.I.P
    Introduces a particle accelerator multiblock, which handles Fusion Reactor recipes, but is capable of doing them in batches of up-to 128, with a 50% discount on energy cost. Commented out for now.
    
    No recipe or texture for the multiblock as of yet. Structure of it is still to be finalized.
    
    Extends custom MI machine definitions to include a 'mod' property, allowing machines to be associated with mods other than 'modern_industrialization'.
    
    Updates asset generation, language entry creation, and loot table generation to utilize the 'mod' property when present.

--- kubejs/startup_scripts/Globals.js
+++ kubejs/startup_scripts/Globals.js
@@ -1,4 +1,4 @@
-// priority: 0
+// priority: 2147483647
 
 /**
  * Enables a few scripts that are only useful for development.
@@ -10,11 +10,13 @@ global.devEnv = false;
  * The list of custom MI machines to generate assets/data for. (Models, sounds, tags, etc.)
  * Possible values for each machine:
  * - id: The machine ID. This is used to reference the machine in the game. REQUIRED.
+ * - mod: The mod ID. This is used to reference the mod for the machine. OPTIONAL. Default: 'modern_industrialization'
  * - casing: The casing tier for the machine. This is used to determine the casing texture for the machine. OPTIONAL. Default: 'lv'
  * - overlay: The overlay ID. This is used to reference the overlay for the machine. OPTIONAL.
  * - hasSides: Whether the machine has side textures. OPTIONAL. Default: false
  * - sound: The sound ID. This is used to reference the sound for the machine. OPTIONAL.
  * - soundDuration: The duration(in ticks) of the sound for the machine. OPTIONAL. Requires sound.
+ * - lang: The language keys for the machine. This is used to create the language entries for the machine. OPTIONAL.
  */
 global.customMIMachines = [
   {
@@ -23,6 +25,9 @@ global.customMIMachines = [
     hasSides: true,
     sound: 'mi_sound_addon:replicator',
     soundDuration: 20,
+    lang: {
+      block: 'Replicator Mk I',
+    },
   },
   {
     id: 'large_plasma_turbine',
@@ -35,7 +40,16 @@ global.customMIMachines = [
   {
     id: 'budding_incubator',
     casing: 'sky_stone_brick_casing',
+    lang: {
+      block: 'Budding Incubator',
+      rei_categories: 'Budding Incubator',
+    },
   },
+  // {
+  //   id: 'particle_accelerator',
+  //   mod: 'mi_tweaks',
+  //   casing: 'plasma_handling_iridium_machine_casing',
+  // },
 ];
 
 Platform.setModName('craftoria', 'Craftoria');
