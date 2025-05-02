e2634ebcb1eb5c4c420395a085930babc30626ca
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-05-02 20:55:51 +0300

    not necessary

--- kubejs/server_scripts/Mods/Craftoria/RecipeFixes.js
+++ kubejs/server_scripts/Mods/Craftoria/RecipeFixes.js
@@ -3,11 +3,6 @@ ServerEvents.recipes(e => {
     e.remove({ id: id });
   });
 
-  e.shaped('cursedearth:blessed_flower', ['FFF', 'FWF', 'FFF'], {
-    F: Ingredient.of('#minecraft:flowers').except('minecraft:wither_rose'),
-    W: 'minecraft:wither_rose',
-  }).id('cursedearth:blessed_flower');
-
   e.replaceInput({}, 'minecraft:crafting_table', '#c:player_workstations/crafting_tables');
 
   e.shapeless('minecraft:pumpkin_pie', ['#c:crops/pumpkin', '#c:sugars', '#c:eggs']).id('minecraft:pumpkin_pie');
