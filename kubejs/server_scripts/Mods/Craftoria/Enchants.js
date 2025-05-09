c3571bb7bfec37227305caee2d056a12abe25ecb
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-05-09 04:55:47 +0300

    Re-enable boon of the earth
    It's easier to balance now, so I'll be taking a look at that later, but at a quick glance, defaults seem *fine*.

--- kubejs/server_scripts/Mods/Craftoria/Enchants.js
+++ kubejs/server_scripts/Mods/Craftoria/Enchants.js
@@ -1,6 +1,6 @@
 ServerEvents.generateData('after_mods', e => {
   /** @type {Special.Enchantment} */
-  const enchantsToDisable = ['apothic_enchanting:boon_of_the_earth', 'ars_elemental:soulbound', 'deeperdarker:catalysis'];
+  const enchantsToDisable = ['ars_elemental:soulbound', 'deeperdarker:catalysis'];
 
   enchantsToDisable.forEach(enchant => {
     let { namespace, path } = ID.mc(enchant);
@@ -15,6 +15,5 @@ ServerEvents.generateData('after_mods', e => {
 });
 
 ServerEvents.tags('enchantment', e => {
-  e.remove('minecraft:non_treasure', ['apothic_enchanting:boon_of_the_earth']);
   e.remove('minecraft:treasure', ['deeperdarker:catalysis']);
 });
