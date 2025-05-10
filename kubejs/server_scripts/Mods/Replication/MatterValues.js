f2d4b94eb2804bb523df48929f5a6fe35753d8c2
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-05-10 17:45:19 +0300

    erm, oops

--- kubejs/server_scripts/Mods/Replication/MatterValues.js
+++ kubejs/server_scripts/Mods/Replication/MatterValues.js
@@ -3,7 +3,7 @@ ServerEvents.recipes(event => {
   const toDisable = ['#c:ingots/iridium', '#c:ingots/platinum', '#c:ingots/titanium', '#c:ingots/tungsten', '#c:ingots/plutonium'];
 
   toDisable.forEach(id => {
-    let { namespace, path } = ID.mc(namespace.startsWith('#') ? id.slice(1) : id);
+    let { namespace, path } = ID.mc(id.startsWith('#') ? id.slice(1) : id);
     event.remove({ id: `replication:matter_values/${namespace}/${id.startsWith('#') ? 'tags' : 'items'}/${path}` });
   });
 });
