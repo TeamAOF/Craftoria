326b0346ecdab9eb441e13c5483f48fe8709b193
Inno <132615999+Kazuhiko-Gushiken@users.noreply.github.com>
2025-05-12 15:46:12 -0700

    added macerator recipe for shattered singualrity to quantum infused dust?

---
+++ kubejs/server_scripts/Mods/ModernIndustrialization/macerator.js
@@ -0,0 +1,7 @@
+ServerRecipes.recipes(e => {
+
+  e.recipes.modern_industrialization.macerator(4, 20)
+    .itemIn("advanced_ae:shattered_singularity")
+    .itemOut("advanced_ae:quantum_infused_dust")
+  
+})
