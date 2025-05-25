d64a8f67b4a6cda537aec7cd11f4b20ec9fd1a0a
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-05-25 03:18:15 +0300

    feat: FD cutting board compat for SushiGoCrafting & Furniture Mod slicing recipes
    closes #562
    
    I would've added these recipes to sushigocrafting board, but that recipe type is weird.
    as for furniture mod cutting board, i'll maybe add that later.

--- kubejs/server_scripts/Mods/FarmersDelight/Recipes.js
+++ kubejs/server_scripts/Mods/FarmersDelight/Recipes.js
@@ -3,45 +3,32 @@ ServerEvents.recipes(e => {
     return _makeRecipeID('farmersdelight', type, input, output);
   };
 
-  let CuttingBoard = (outputs, inputs, tool, isAbility, sound) => {
+  /**
+   *
+   * @param {$ItemStack_|$ItemStack_[]} outputs
+   * @param {$Ingredient_|$Ingredient_[]} inputs
+   * @param {$Ingredient_} tool
+   * @param {boolean} isAbility
+   * @param {string} sound
+   */
+  let cuttingBoard = (outputs, inputs, tool, isAbility, sound) => {
     let recipe = {
       type: 'farmersdelight:cutting',
       ingredients: [],
       result: [],
-      tool: {},
+      tool: isAbility ? { type: 'farmersdelight:item_ability', action: tool } : Ingredient.of(tool).toJson()
     };
-    let recipeID = [];
     if (sound) recipe.sound = { sound_id: sound };
 
-    if (Array.isArray(outputs)) {
-      outputs.forEach(output => {
-        recipe.result.push({ item: Item.of(output).toJson() });
-      });
-      recipeID.push(outputs[0]);
-    } else {
-      recipe.result.push({ item: Item.of(outputs).toJson() });
-      recipeID.push(outputs);
-    }
+    // Handle outputs
+    let outputItems = Array.isArray(outputs) ? outputs : [outputs];
+    recipe.result = outputItems.map(output => ({ item: Item.of(output).toJson() }));
 
-    if (Array.isArray(inputs)) {
-      inputs.forEach(input => {
-        recipe.ingredients.push(Ingredient.of(input).toJson());
-      });
-      recipeID.push(inputs[0]);
-    } else {
-      recipe.ingredients.push(Ingredient.of(inputs).toJson());
-      recipeID.push(inputs);
-    }
+    // Handle inputs
+    let inputItems = Array.isArray(inputs) ? inputs : [inputs];
+    recipe.ingredients = inputItems.map(input => Ingredient.of(input).toJson());
 
-    if (isAbility) {
-      recipe.tool = {
-        type: 'farmersdelight:item_ability',
-        action: tool,
-      };
-    } else {
-      recipe.tool = Ingredient.of(tool).toJson();
-    }
-    e.custom(recipe).id(makeRecipeID('cutting', recipeID[0], recipeID[1]));
+    e.custom(recipe).id(makeRecipeID('cutting', outputItems[0], inputItems[0]));
   };
 
   ['minecraft:bread', 'moredelight:bread_slice'].forEach(toastItem => {
@@ -55,4 +42,30 @@ ServerEvents.recipes(e => {
       },
     });
   });
+
+  e.forEachRecipe({ type:'refurbished_furniture:cutting_board_slicing' }, kubeRecipe => {
+    let recipeJson = JSON.parse(kubeRecipe.json.toString());
+    let { result: { id: resultId, count: resultCount }, ingredient: { item: ingredient } } = recipeJson;
+
+    if (!Item.exists(resultId) || !Item.exists(ingredient)) {
+      logError(`${resultId} or ${ingredient} does not exist`, recipeJson);
+      return;
+    }
+
+    cuttingBoard(`${resultCount || 1}x ${resultId}`, ingredient, '#c:tools/knife');
+  });
+
+  /** @type {Record<$Ingredient_, Special.Item>} */
+  const sushigocrafting = {
+    'minecraft:cod' : 'sushigocrafting:imitation_crab',
+    '#c:fruits/avocado' : 'sushigocrafting:avocado_slices',
+    '#c:crops/cucumber' : 'sushigocrafting:cucumber_slices',
+    '#c:crops/wasabi_root' : 'sushigocrafting:wasabi_paste',
+    '#c:raw_fishes/tuna' : 'sushigocrafting:tuna_fillet',
+    '#c:raw_fishes/salmon' : 'sushigocrafting:salmon_fillet',
+  };
+
+  for (const [input, output] of Object.entries(sushigocrafting)) {
+    cuttingBoard(output, input, '#c:tools/knife');
+  }
 });
