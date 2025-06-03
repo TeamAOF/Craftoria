2293d855f21cd3a666d408f0868c84e2d6971fd1
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-06-03 12:43:59 +0300

    feat(recipe): Add Xychorium ores to MI quarry (Bronze Drill); Add Kivi to MI quarry (Copper Drill)
    also refactors quarry recipes to use MIs built in KJS compat.

--- kubejs/server_scripts/Mods/ModernIndustrialization/Quarry.js
+++ kubejs/server_scripts/Mods/ModernIndustrialization/Quarry.js
@@ -3,154 +3,65 @@
 // //////////////////////
 
 ServerEvents.recipes(event => {
-  let miQuarryRecipeIDs = [];
+  const { quarry } = event.recipes.modern_industrialization
 
-  event.forEachRecipe({ type: 'modern_industrialization:quarry' }, recipe => {
-    let id = `${recipe.id}`;
-    if (!id.includes('modern_industrialization:quarry/')) return;
-    miQuarryRecipeIDs.push(id);
-  });
-
-  /**
-   * Modern Industrialization Quarry Recipe
-   * @param {MIItem} input Add the input item, amount and probability(1 being 100%, 0.01 being 1%) as an array.
-   * @param {MIItem[]} outputs Add the output items, amount and probability(1 being 100%, 0.01 being 1%) as an array of arrays.
-   * @param {number} eu EU/t consumed by the machine.
-   * @param {number} duration Duration of the recipe in ticks.
-   * @param {boolean} overwrite Should the recipe overwrite the existing one. (Default: true) Usefull for updating recipes.
-   */
-  let MIQuarry = (input, outputs, eu, duration, overwrite) => {
-    let recipe = {
-      type: 'modern_industrialization:quarry',
-      eu: eu,
-      duration: duration,
-      item_inputs: {},
-      item_outputs: [],
-    };
-    if (input.length != 3 || outputs.length == 0) {
-      logError(`Invalid Quarry Recipe: ${input[0]} is invalid, check the recipe format.`);
-      return;
-    }
-    recipe.item_inputs.item = input[0];
-    recipe.item_inputs.amount = input[1];
-    recipe.item_inputs.probability = input[2];
-
-    outputs.forEach(output => {
-      if (output.length != 3) {
-        logError(`Invalid Quarry Recipe: ${input[0]}. ${output[0]} is invalid, check the recipe format.`);
-        return;
-      }
-
-      if (Item.exists(output[0]) == false) {
-        logWarn(`Invalid Quarry Recipe: ${input[0]}. ${output[0]} does not exist.`);
-        return;
-      }
-
-      let item = {
-        item: output[0],
-        amount: output[1],
-        probability: output[2],
-      };
-      recipe.item_outputs.push(item);
-    });
-
-    overwrite = overwrite == undefined ? true : overwrite;
-    if (overwrite) {
-      let tempID = `modern_industrialization:quarry/${input[0].split(':')[1]}`;
-      tempID = tempID.replace('_drill', '');
-      miQuarryRecipeIDs.forEach(recipe => {
-        if (!recipe.includes(tempID)) return;
-
-        // console.log(`Removing Recipe: ${tempID}`);
-        event.remove({ id: tempID });
-      });
-    }
-
-    event.custom(recipe).id(`craftoria:modern_industrialization/quarry/${input[0].split(':')[1]}`);
-  };
+  // Copper Drill
+  quarry(4, 600)
+    .itemIn('modern_industrialization:copper_drill', 0.1)
+    .itemOut('32x minecraft:cobblestone', 0.5)
+    .itemOut('16x minecraft:diorite', 0.25)
+    .itemOut('16x minecraft:andesite', 0.25)
+    .itemOut('16x minecraft:granite', 0.25)
+    .itemOut('10x minecraft:dirt', 0.5)
+    .itemOut('10x minecraft:gravel', 0.5)
+    .itemOut('16x minecraft:cobbled_deepslate', 0.25)
+    .itemOut('16x minecraft:tuff', 0.25)
+    .itemOut('16x xycraft_world:kivi', 0.25)
+    .id('modern_industrialization:quarry/copper');
 
   // Steel Drill
-  MIQuarry(
-    ['modern_industrialization:steel_drill', 1, 0.04],
-    [
-      ['modern_industrialization:antimony_ore', 1, 0.2],
-      ['minecraft:diamond_ore', 1, 0.12],
-      ['minecraft:lapis_ore', 1, 0.1],
-      ['modern_industrialization:lead_ore', 1, 0.25],
-      ['modern_industrialization:nickel_ore', 1, 0.18],
-      ['modern_industrialization:bauxite_ore', 1, 0.4],
-      ['modern_industrialization:salt_ore', 1, 0.12],
-      ['minecraft:emerald_ore', 1, 0.1],
-      ['modern_industrialization:quartz_ore', 1, 0.2],
-      ['powah:uraninite_ore', 1, 0.08],
-      ['actuallyadditions:black_quartz_ore', 1, 0.15],
-      ['create:zinc_ore', 1, 0.2],
-    ],
-    12,
-    600
-  );
-
-  // Gold Drill
-  MIQuarry(
-    ['modern_industrialization:gold_drill', 1, 0.1],
-    [
-      ['minecraft:netherrack', 32, 0.5],
-      ['minecraft:blackstone', 16, 0.25],
-      ['minecraft:basalt', 16, 0.25],
-      ['minecraft:soul_soil', 20, 0.1],
-      ['minecraft:magma_block', 5, 0.1],
-      ['minecraft:soul_sand', 20, 0.25],
-      ['minecraft:ancient_debris', 1, 0.05],
-      ['minecraft:glowstone', 4, 0.15],
-      ['minecraft:nether_quartz_ore', 6, 0.15],
-      ['minecraft:nether_gold_ore', 1, 0.25],
-    ],
-    8,
-    600
-  );
+  quarry(12, 600)
+    .itemIn('modern_industrialization:steel_drill', 0.04)
+    .itemOut('modern_industrialization:antimony_ore', 0.2)
+    .itemOut('minecraft:diamond_ore', 0.12)
+    .itemOut('minecraft:lapis_ore', 0.1)
+    .itemOut('modern_industrialization:lead_ore', 0.25)
+    .itemOut('modern_industrialization:nickel_ore', 0.18)
+    .itemOut('modern_industrialization:bauxite_ore', 0.4)
+    .itemOut('modern_industrialization:salt_ore', 0.12)
+    .itemOut('minecraft:emerald_ore', 0.1)
+    .itemOut('modern_industrialization:quartz_ore', 0.2)
+    .itemOut('powah:uraninite_ore', 0.08)
+    .itemOut('actuallyadditions:black_quartz_ore', 0.15)
+    .itemOut('create:zinc_ore', 0.2)
+    .id('modern_industrialization:quarry/steel');
 
   // Bronze Drill
-  MIQuarry(
-    ['modern_industrialization:bronze_drill', 1, 0.04],
-    [
-      ['minecraft:iron_ore', 1, 0.4],
-      ['minecraft:coal_ore', 1, 0.4],
-      ['modern_industrialization:lignite_coal_ore', 1, 0.24],
-      ['minecraft:copper_ore', 1, 0.2],
-      ['modern_industrialization:tin_ore', 1, 0.3],
-      ['occultism:silver_ore', 1, 0.15],
-      ['minecraft:gold_ore', 1, 0.15],
-      ['minecraft:redstone_ore', 1, 0.2],
-      ['mekanism:osmium_ore', 1, 0.12],
-    ],
-    4,
-    600
-  );
+  quarry(4, 600)
+    .itemIn('modern_industrialization:bronze_drill', 0.04)
+    .itemOut('minecraft:iron_ore', 0.4)
+    .itemOut('minecraft:coal_ore', 0.4)
+    .itemOut('modern_industrialization:lignite_coal_ore', 0.24)
+    .itemOut('minecraft:copper_ore', 0.2)
+    .itemOut('modern_industrialization:tin_ore', 0.3)
+    .itemOut('occultism:silver_ore', 0.15)
+    .itemOut('minecraft:gold_ore', 0.15)
+    .itemOut('minecraft:redstone_ore', 0.2)
+    .itemOut('mekanism:osmium_ore', 0.12)
+    .itemOut('xycraft_world:xychorium_ore_stone_blue', 0.4)
+    .itemOut('xycraft_world:xychorium_ore_stone_green', 0.4)
+    .itemOut('xycraft_world:xychorium_ore_stone_red', 0.4)
+    .itemOut('xycraft_world:xychorium_ore_stone_dark', 0.4)
+    .itemOut('xycraft_world:xychorium_ore_stone_light', 0.4)
+    .id('modern_industrialization:quarry/bronze');
 
   // Titanium Drill
-  MIQuarry(
-    ['modern_industrialization:titanium_drill', 1, 0.04],
-    [
-      ['modern_industrialization:uranium_ore', 1, 0.2],
-      ['modern_industrialization:iridium_ore', 1, 0.05],
-      ['powah:uraninite_ore_dense', 1, 0.02],
-      ['mekanism:fluorite_ore', 1, 0.3],
-      ['irons_spellbooks:mithril_ore', 1, 0.02],
-    ],
-    128,
-    600
-  );
-
-  // Stainless Steel Drill
-  MIQuarry(
-    ['modern_industrialization:stainless_steel_drill', 1, 0.04],
-    [
-      ['modern_industrialization:titanium_ore', 1, 0.15],
-      ['modern_industrialization:tungsten_ore', 1, 0.2],
-      ['modern_industrialization:monazite_ore', 1, 0.25],
-      ['modern_industrialization:platinum_ore', 1, 0.12],
-    ],
-    32,
-    600
-  );
+  quarry(128, 600)
+    .itemIn('modern_industrialization:titanium_drill', 0.04)
+    .itemOut('modern_industrialization:uranium_ore', 0.2)
+    .itemOut('modern_industrialization:iridium_ore', 0.05)
+    .itemOut('powah:uraninite_ore_dense', 0.02)
+    .itemOut('mekanism:fluorite_ore', 0.3)
+    .itemOut('irons_spellbooks:mithril_ore', 0.02)
+    .id('modern_industrialization:quarry/titanium');
 });
