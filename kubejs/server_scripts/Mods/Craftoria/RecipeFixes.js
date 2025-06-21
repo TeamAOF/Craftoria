3cdbef44b4ce9271430c6be781a68ee71d5dbc90
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-06-21 15:42:42 +0300

    feat(recipe): made a bunch of recipes use wooden chests, instead of any chest
    maybe you could categorise this as a bug fix? idk, many mod devs seem to use any chest tag in their recipes, which includes many expensive chests too, and you wouldn't want to use those in recipes, no?

--- kubejs/server_scripts/Mods/Craftoria/RecipeFixes.js
+++ kubejs/server_scripts/Mods/Craftoria/RecipeFixes.js
@@ -11,4 +11,43 @@ ServerEvents.recipes(e => {
   }).id('craftoria:rice_dough');
 
   e.replaceInput({ id: 'occultism:crushing/coal_dust_from_tag' }, '#minecraft:coals', 'minecraft:coal');
+
+  /** @type {Special.RecipeId[]} */
+  const toWooden = [
+    'sfm:manager',
+    'sfm:cable',
+    'sushigocrafting:cooler_box',
+    'occultism:crafting/book_of_calling_foliot_transport_items',
+    'advancedperipherals:inventory_manager',
+    'advancedperipherals:nbt_storage',
+    'sophisticatedbackpacks:crafting_upgrade',
+    'morefunctionalstorage:refill_upgrade',
+    'laserio:card_holder',
+    'mekanism_extras:upgrade/stack',
+    'aquaculture:tackle_box',
+    'ars_nouveau:allow_scroll',
+    'xycraft_machines:shaped/fabricator',
+    'sophisticatedstorage:shulker_box',
+    'rftoolsutility:invchecker',
+    'rftoolsutility:inventory_module',
+    'occultism:ritual/summon_foliot_cleaner',
+    'occultism:ritual/familiar_greedy',
+    'occultism:ritual/summon_foliot_transport_items',
+  ];
+
+  toWooden.forEach(id => {
+    e.replaceInput({ id: id }, '#c:chests', '#c:chests/wooden');
+  });
+
+  const toWoodenApparatus = [
+    'ars_nouveau:storage_lectern',
+    'ars_nouveau:relay_collector',
+  ];
+
+  e.forEachRecipe({ type: 'ars_nouveau:enchanting_apparatus' }, kubeRecipe => {
+    if (!toWoodenApparatus.includes(kubeRecipe.getId())) return;
+    let recipeJson = JSON.parse(kubeRecipe.json);
+    recipeJson.pedestalItems.forEach(entry => { if (entry.tag === 'c:chests') entry.tag = 'c:chests/wooden'; });
+    e.custom(recipeJson).id(kubeRecipe.getId());
+  });
 });
