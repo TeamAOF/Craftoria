4bfb6413125e76107f4a312edd620373b47ce5d3
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-07-08 15:07:02 +0300

    i should quit coke
    the drink i mean,
    
    probably was leftover from when this actually needed the while loop, i've changed this script so many times while just trying to get it to work right that idk anymore

--- kubejs/server_scripts/Dev/GenerateMIMulti.js
+++ kubejs/server_scripts/Dev/GenerateMIMulti.js
@@ -70,16 +70,11 @@ function getCharForBlock(blockId, block, mapping, charCodeRef, player) {
         return null;
       }
     } else if (blockId !== 'minecraft:air') {
-      while (true) {
-        if (charCodeRef.value === 91) { // charCode for '[' (after 'Z')
-          charCodeRef.value = 97; // charCode for 'a'
-          continue; // Re-evaluate
-        }
-        if (charCodeRef.value === 123) { // charCode for '{' (after 'z') - Ran out of A-Z, a-z
-          player.tell(Text.red(`Too many unique non-controller block types (${mapping.size}). Ran out of A-Z, a-z characters.`));
-          return null; // Ran out of A-Z, a-z characters, abort. Why do you need more than 52? Too complex IMHO.
-        }
-        break; // Character found and valid
+      if (charCodeRef.value === 91) { // charCode for '[' (after 'Z')
+        charCodeRef.value = 97; // charCode for 'a'
+      } else if (charCodeRef.value === 123) { // charCode for '{' (after 'z') - Ran out of A-Z, a-z
+        player.tell(Text.red(`Too many unique non-controller block types (${mapping.size}). Ran out of A-Z, a-z characters.`));
+        return null; // Ran out of A-Z, a-z characters, abort. Why do you need more than 52? Too complex IMHO.
       }
 
       // Check mapping size limit (52 unique chars: A-Z, a-z)
