226a5c481866dfe4d2d95a6770934dc113e09731
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-07-13 18:37:56 +0300

    yep

--- kubejs/server_scripts/helpers/industrial_foregoing.js
+++ kubejs/server_scripts/helpers/industrial_foregoing.js
@@ -13,6 +13,72 @@
  */
 
 /**
+ * @callback RarityCallback
+ * @param {LaserDrillRarity} rarity - Rarity configuration
+ * @returns {LaserDrillBuilder}
+ */
+
+/**
+ * @callback RaritiesCallback
+ * @param {LaserDrillRarity[]} rarityList - Array of rarity configurations
+ * @returns {LaserDrillBuilder}
+ */
+
+/**
+ * @callback ConfiguredRarityCallback
+ * @param {number} weight - Weight for this rarity
+ * @param {Object} [options] - Configuration options
+ * @param {number} [options.depthMin] - Minimum depth (default: 5)
+ * @param {number} [options.depthMax] - Maximum depth (default: 255)
+ * @param {Special.WorldgenBiome[]} [options.biomeWhitelist] - Biomes to whitelist
+ * @param {Special.WorldgenBiome[]} [options.biomeBlacklist] - Biomes to blacklist
+ * @param {Special.Dimension[]} [options.dimensionWhitelist] - Dimensions to whitelist
+ * @param {Special.Dimension[]} [options.dimensionBlacklist] - Dimensions to blacklist
+ * @returns {LaserDrillBuilder}
+ */
+
+/**
+ * @callback BiomeRarityCallback
+ * @param {Special.WorldgenBiome[]} biomes - Biomes to whitelist
+ * @param {number} weight - Weight for this rarity
+ * @param {number} [depthMin] - Minimum depth (default: 5)
+ * @param {number} [depthMax] - Maximum depth (default: 255)
+ * @returns {LaserDrillBuilder}
+ */
+
+/**
+ * @callback DimensionRarityCallback
+ * @param {Special.Dimension[]} dimensions - Dimensions to whitelist
+ * @param {number} weight - Weight for this rarity
+ * @param {number} [depthMin] - Minimum depth (default: 5)
+ * @param {number} [depthMax] - Maximum depth (default: 255)
+ * @returns {LaserDrillBuilder}
+ */
+
+/**
+ * @callback IdCallback
+ * @param {string} recipeId - The recipe ID
+ * @returns {LaserDrillBuilder}
+ */
+
+/**
+ * @callback RegisterCallback
+ * @returns {LaserDrillBuilder}
+ */
+
+/**
+ * @typedef {Object} LaserDrillBuilder
+ * @property {RarityCallback} rarity - Add a rarity configuration
+ * @property {RaritiesCallback} rarities - Add multiple rarity configurations
+ * @property {RegisterCallback} defaultRarities - Add default overworld rarity configurations
+ * @property {ConfiguredRarityCallback} configuredRarity - Add a rarity configuration with flexible biome and dimension filtering
+ * @property {BiomeRarityCallback} biomeRarity - Add biome-specific rarity
+ * @property {DimensionRarityCallback} dimensionRarity - Add dimension-specific rarity
+ * @property {IdCallback} id - Set the recipe ID
+ * @property {RegisterCallback} register - Register with auto-generated ID
+ */
+
+/**
  * Industrial Foregoing Recipe Helpers
  * @param {$RecipesKubeEvent_} event
  */
@@ -98,50 +164,163 @@ function IndustrialForegoingHelper(event) {
     },
 
     /**
-     * @param {Special.Item} itemOutput
-     * @param {Special.Item} catalyst
-     * @param {LaserDrillRarity[] | LaserDrillRarity} [rarityList]
-     * @param {Special.RecipeId} [recipeID] The recipe ID, can be used to overwrite recipes (optional, default is generated based on recipe parameters).
+     * Creates a chainable recipe builder for Laser Drill Ore recipes
+     * @param {Special.Item} itemOutput - Item identifier for the output
+     * @param {Special.Item} catalyst - Catalyst item identifier
+     * @returns {LaserDrillBuilder}
      */
-    oreLaserDrilling(itemOutput, catalyst, rarityList, recipeID) {
-      let recipe = {
+    oreLaserDrilling(itemOutput, catalyst) {
+      const recipe = {
         type: 'industrialforegoing:laser_drill_ore',
-        catalyst: Ingredient.of(catalyst).toJson(),
         output: Ingredient.of(itemOutput).toJson(),
+        catalyst: Ingredient.of(catalyst).toJson(),
         rarity: [],
       };
 
-      if (rarityList) {
-        let rarities = Array.isArray(rarityList) ? rarityList : [rarityList];
-        rarities.forEach(r => {
-          recipe.rarity.push(r);
-        });
-      } else {
-        recipe.rarity.push(
-          {
-            biome_filter: { blacklist: [], whitelist: [] },
-            depth_max: 132,
-            depth_min: 5,
-            dimension_filter: {
-              blacklist: ['minecraft:the_end'],
-              whitelist: [],
-            },
-            weight: 10,
-          },
-          {
-            biome_filter: { blacklist: [], whitelist: [] },
-            depth_max: 255,
-            depth_min: 0,
-            dimension_filter: {
-              blacklist: ['minecraft:the_end'],
-              whitelist: [],
-            },
-            weight: 4,
-          }
-        );
-      }
+      const builder = {
 
-      event.custom(recipe).id(recipeID ?? makeRecipeId('laser_drill_ore', itemOutput));
+        /**
+         * Add a rarity configuration to the recipe
+         * @param {LaserDrillRarity} rarity - Rarity configuration
+         * @returns {LaserDrillBuilder}
+         */
+        rarity(rarity) {
+          recipe.rarity.push(rarity);
+          return builder;
+        },
+
+        /**
+         * Add multiple rarity configurations to the recipe
+         * @param {LaserDrillRarity[]} rarityList - Array of rarity configurations
+         * @returns {LaserDrillBuilder}
+         */
+        rarities(rarityList) {
+          rarityList.forEach(r => recipe.rarity.push(r));
+          return builder;
+        },
+
+        /**
+         * Add default rarity configurations for overworld mining
+         * @returns {LaserDrillBuilder}
+         */
+        defaultRarities() {
+          recipe.rarity.push(
+            {
+              biome_filter: { blacklist: [], whitelist: [] },
+              depth_max: 132,
+              depth_min: 5,
+              dimension_filter: {
+                blacklist: ['minecraft:the_end'],
+                whitelist: [],
+              },
+              weight: 10,
+            },
+            {
+              biome_filter: { blacklist: [], whitelist: [] },
+              depth_max: 255,
+              depth_min: 0,
+              dimension_filter: {
+                blacklist: ['minecraft:the_end'],
+                whitelist: [],
+              },
+              weight: 4,
+            }
+          );
+          return builder;
+        },
+
+        /**
+         * Add a rarity configuration with flexible biome and dimension filtering
+         * @param {number} weight - Weight for this rarity
+         * @param {Object} [options] - Configuration options
+         * @param {number} [options.depthMin] - Minimum depth (default: 5)
+         * @param {number} [options.depthMax] - Maximum depth (default: 255)
+         * @param {Special.WorldgenBiome[]} [options.biomeWhitelist] - Biomes to whitelist
+         * @param {Special.WorldgenBiome[]} [options.biomeBlacklist] - Biomes to blacklist
+         * @param {Special.Dimension[]} [options.dimensionWhitelist] - Dimensions to whitelist
+         * @param {Special.Dimension[]} [options.dimensionBlacklist] - Dimensions to blacklist
+         * @returns {LaserDrillBuilder}
+         */
+        configuredRarity(weight, options) {
+          options = options || {};
+
+          const rarityConfig = {
+            biome_filter: {
+              blacklist: options.biomeBlacklist || [],
+              whitelist: options.biomeWhitelist || []
+            },
+            depth_max: options.depthMax || 255,
+            depth_min: options.depthMin || 5,
+            dimension_filter: {
+              blacklist: options.dimensionBlacklist || [],
+              whitelist: options.dimensionWhitelist || []
+            },
+            weight: weight,
+          };
+
+          recipe.rarity.push(rarityConfig);
+          return builder;
+        },
+
+        /**
+         * Add a rarity configuration for specific biomes (whitelist only - convenience method)
+         * @param {Special.WorldgenBiome[]} biomes - Biomes to whitelist
+         * @param {number} weight - Weight for this rarity
+         * @param {number} [depthMin] - Minimum depth (default: 5)
+         * @param {number} [depthMax] - Maximum depth (default: 255)
+         * @returns {LaserDrillBuilder}
+         */
+        biomeRarity(biomes, weight, depthMin, depthMax) {
+          return builder.configuredRarity(weight, {
+            biomeWhitelist: biomes,
+            depthMin: depthMin,
+            depthMax: depthMax
+          });
+        },
+
+        /**
+         * Add a rarity configuration for specific dimensions (whitelist only - convenience method)
+         * @param {Special.Dimension[]} dimensions - Dimensions to whitelist
+         * @param {number} weight - Weight for this rarity
+         * @param {number} [depthMin] - Minimum depth (default: 5)
+         * @param {number} [depthMax] - Maximum depth (default: 255)
+         * @returns {LaserDrillBuilder}
+         */
+        dimensionRarity(dimensions, weight, depthMin, depthMax) {
+          return builder.configuredRarity(weight, {
+            dimensionWhitelist: dimensions,
+            depthMin: depthMin,
+            depthMax: depthMax
+          });
+        },
+
+        /**
+         * Set the recipe ID
+         * @param {string} recipeId - The recipe ID
+         * @returns {LaserDrillBuilder}
+         */
+        id(recipeId) {
+          event.custom(recipe).id(recipeId);
+          return builder;
+        },
+
+        /**
+         * Register the recipe with an auto-generated ID
+         * @returns {LaserDrillBuilder}
+         */
+        register() {
+          // Use default rarities if none were specified
+          if (recipe.rarity.length === 0) {
+            builder.defaultRarities();
+          }
+
+          const autoId = makeRecipeId('laser_drill_ore', recipe.output?.item || recipe.output);
+          event.custom(recipe).id(autoId);
+          return builder;
+        },
+      };
+
+      return builder;
     },
   };
 }
--- kubejs/server_scripts/mods/industrial_foregoing/recipes.js
+++ kubejs/server_scripts/mods/industrial_foregoing/recipes.js
@@ -12,24 +12,6 @@ ServerEvents.recipes(e => {
   foregoing.fluidExtraction('1x justdirethings:xp_fluid_source', 'minecraft:sculk', 'minecraft:air', 0.1);
   foregoing.fluidExtraction('1x pneumaticcraft:yeast_culture', '#c:mushroom_blocks', 'minecraft:air', 0.01);
 
-  foregoing.oreLaserDrilling(
-    'minecraft:ancient_debris',
-    'industrialforegoing:brown_laser_lens',
-    [
-      {
-        biome_filter: {
-          blacklist: [],
-          whitelist: [],
-        },
-        depth_max: 20,
-        depth_min: 1,
-        dimension_filter: {
-          blacklist: [],
-          whitelist: ['minecraft:the_nether', 'jamd:nether'],
-        },
-        weight: 1,
-      },
-    ],
-    'industrialforegoing:laser_drill_ore/ancient_debris'
-  );
+  foregoing.oreLaserDrilling('minecraft:ancient_debris', 'industrialforegoing:brown_laser_lens')
+    .dimensionRarity(['minecraft:the_nether', 'jamd:nether'], 1, 1, 20).id('industrialforegoing:laser_drill_ore/ancient_debris');
 });
