7d2fc0471aa77ae5a0f289830b3373eabf32c827
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-07-13 18:37:08 +0300

    sniffa

--- kubejs/server_scripts/craftoria/loot.js
+++ kubejs/server_scripts/craftoria/loot.js
@@ -16,8 +16,20 @@ LootJS.lootTables(e => {
   e.getEntityTable('turtle')
     .createPool()
     .addEntry(LootEntry.of('turtle_scute').setCount([0, 3]).applyEnchantmentBonus('looting', [0, 1]));
+
+  const sniffa = e.getLootTable('minecraft:gameplay/sniffer_digging').firstPool();
+  [
+    'biomeswevegone:fluorescent_cattail_sprout',
+    'biomeswevegone:blue_glowcane_shoot',
+    'biomeswevegone:green_glowcane_shoot',
+    'biomeswevegone:red_glowcane_shoot',
+    'biomeswevegone:yellow_glowcane_shoot',
+    'biomeswevegone:pale_pumpkin_seeds'
+  ].forEach(item => {
+    sniffa.addEntry(item);
+  });
 });
 
 LootJS.modifiers(e => {
-  e.removeGlobalModifiers('dumplings_delight:add_calamari');
+  e.removeGlobalModifiers(['dumplings_delight:add_calamari', 'biomeswevegone:bwg_items_from_sniffer_dig']);
 });
