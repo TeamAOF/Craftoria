0504618dda3dee803af7ccd2bf8a2b12538d7fa9
JohnTrinca <45481896+JohnTrinca@users.noreply.github.com>
2025-08-21 18:10:07 +0200

    aggiunte recipe conversione e descrizione del drop

--- kubejs/server_scripts/trophies.js
+++ kubejs/server_scripts/trophies.js
@@ -74,3 +74,30 @@ ServerEvents.generateData('after_mods', event => {
   // Scrivo il file completo per tutti i mob Cataclysm
   event.json(`trofers:trofers/entity_drops/mod_drops`, drops);
 });
+
+// add a description on how the trophies are dropped
+// I only added it on the generic small plate that is the one that normally drops 
+RecipeViewerEvents.addInformation('item', event => {
+	event.add('trofers:small_plate', [
+		'Each enemy has a low chance of dropping its trophy upon death.'
+	]);
+})
+
+// added all the recipe for plate conversion in the stonecutter
+ServerEvents.recipes(event => {
+  const trophies = JsonIO.read('kubejs/data/trofers/trophies.json');
+
+  Object.entries(trophies).forEach(([id, data]) => {
+    const trophy_name = `${data.folder}/${id}`;
+
+    const trophy_type = ['small_plate','small_pillar','medium_plate','medium_pillar','large_plate','large_pillar'];
+
+    trophy_type.forEach(input => {
+      trophy_type.forEach(output => {
+        if(input!=output){
+          event.stonecutting(`trofers:${input}[trofers:trophy="trofers:${trophy_name}"]`, `trofers:${output}[trofers:trophy="trofers:${trophy_name}"]`);
+        }
+      });
+    });
+  });
+});
