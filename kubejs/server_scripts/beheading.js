f7e0213b94cef31bb847dc7503137934c645a270
JohnTrinca <45481896+JohnTrinca@users.noreply.github.com>
2025-08-25 09:23:26 +0200

    error fix

--- kubejs/server_scripts/beheading.js
+++ kubejs/server_scripts/beheading.js
@@ -1,16 +1,18 @@
 EntityEvents.death("player", (event) => {
   console.log("player morto");
-  
-  const { entity, source } = event;
+
+  let { entity, source } = event;
 
   //console.log(`tentativo di drop: minecraft:player_head[minecraft:profile="${entity.name.string}"]`);
-  //console.log(entity.getType());
-
+  
   //dropchance in percentage
-  const dropChance = 0.5 
+  let dropChance = 0.5 
+  
+  // console.log("UCCISORE:" + source.getType());
+  let attacker = source.actual;
   
   // Controlla se la causa della morte è un altro giocatore
-  if (source.getType() == "minecraft:player" && Math.random() < dropChance) {
+  if (attacker.isPlayer() && Math.random() < dropChance) {
     let myEntity = event.level.createEntity('item');
     myEntity.item = Item.of(`minecraft:player_head[minecraft:profile="${entity.name.string}"]`);
     myEntity.x = entity.x;
