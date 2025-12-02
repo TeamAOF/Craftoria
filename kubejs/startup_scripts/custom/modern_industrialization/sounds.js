855fa2eaca65ec9738edd563eefcb862bdd08cf4
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-12-03 01:54:13 +0200

    it brok i fix

--- kubejs/startup_scripts/custom/modern_industrialization/sounds.js
+++ kubejs/startup_scripts/custom/modern_industrialization/sounds.js
@@ -1,19 +1,19 @@
-MISoundAddons.modifySounds(e => {
-  let duration = {
-    bending_machine: 23,
-    alloy_smelter: 38,
-    canning_machine: 56,
-    composter: 82,
-    brewery: 71,
-  };
+// MISoundAddons.modifySounds(e => {
+//   let duration = {
+//     bending_machine: 23,
+//     alloy_smelter: 38,
+//     canning_machine: 56,
+//     composter: 82,
+//     brewery: 71,
+//   };
 
-  for (let [id, machine] of Object.entries(global.customMIMachines)) {
-    if (machine.sound && machine.soundDuration) {
-      duration[id] = machine.soundDuration;
-    }
-  }
+//   for (let [id, machine] of Object.entries(global.customMIMachines)) {
+//     if (machine.sound && machine.soundDuration) {
+//       duration[id] = machine.soundDuration;
+//     }
+//   }
 
-  Object.keys(duration).forEach(key => {
-    e.modifyDuration(key, duration[key]);
-  });
-});
+//   Object.keys(duration).forEach(key => {
+//     e.modifyDuration(key, duration[key]);
+//   });
+// });
