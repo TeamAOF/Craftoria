7ad730c35373de50e2395d0b1f06aa7877212d69
Kazuhiko-Gushiken <just.a.stem.nerd@gmail.com>
2025-12-04 15:47:07 -0800

    added reversable recipes for dyed ae2 cables, smart and covered

--- kubejs/server_scripts/Mods/AE2/Recipes.js
+++ kubejs/server_scripts/Mods/AE2/Recipes.js
@@ -76,4 +76,29 @@ ServerEvents.recipes(event => {
   toAE2Metal.forEach(id => {
     event.replaceInput({ id: id }, 'minecraft:iron_ingot', '#ae2:metal_ingots');
   });
+
+  
+  // added reversable recipes for dyed ae2 cables, covered and not. - inno
+  const colors = [
+    'white', 'light_gray', 'gray', 'black', 'lime', 'yellow', 'orange', 'brown', 'red', 'pink', 'magenta', 'purple', 'blue', 'light_blue', 'cyan', 'green'
+  ];
+
+  const reversablePairs = [
+    { a: "smart_cable", b: "smart_dense_cable", sCount: 4, dCount: 1},
+    { a: "covered_cable", b: "covered_dense_cable", sCount: 4, dCount: 1}
+  ];
+
+  colors.forEach(color => {
+    reversablePairs.forEach(pair => {
+      event.shapeless(
+        `${pair.sCount}x ae2:${color}_${pair.a}`,
+        [`${pair.dCount}x ae2:${color}_${pair.b}`]
+      );
+
+      event.shapeless(
+        `${pair.dCount}x ae2:${color}_${pair.b}`,
+        [`${pair.sCount}x ae2:${color}_${pair.a}`]
+      );
+    });
+  });
 });
