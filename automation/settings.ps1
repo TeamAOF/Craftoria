295bd8fb5c7d5f347c0c702c3aac930971cd2b7d
AK <anthonkrisat@hotmail.com>
2025-12-11 16:53:21 +0100

    Revert "version bump 1.29.0"
    This reverts commit 20d9640cd98a9d40afdae163634f1cf1ca3a20d2.

--- automation/settings.ps1
+++ automation/settings.ps1
@@ -26,11 +26,11 @@ $MODPACK_NAME = "Craftoria"
 $CLIENT_NAME = "Craftoria"
 
 # Version Of The Modpack
-$MODPACK_VERSION = "1.29.0"
+$MODPACK_VERSION = "1.28.0"
 
 # Last Version Of The Modpack
 # Needed For Changelog Parsing
-$LAST_MODPACK_VERSION = "1.28.0"
+$LAST_MODPACK_VERSION = "1.27.0"
 
 # Which modloader the modpack uses
 # Use proper capitalization for display (e.g., "NeoForge", "Fabric", "Forge")
