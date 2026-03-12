24479a755aa979bd162df75f170521caa015c691
Kaiser <133273705+KaiserHua1901@users.noreply.github.com>
2026-03-11 18:02:58 -0700

    feat: added all MI plate to create press
    exclude nuclear alloy, blastproof, iridium, diamond and emerald plate

--- kubejs/server_scripts/Mods/Create/recipes.js
+++ kubejs/server_scripts/Mods/Create/recipes.js
@@ -13,8 +13,17 @@ ServerEvents.recipes(e => {
 
   e.smelting('modern_industrialization:silver_ingot', 'create:crushed_raw_silver');
   e.smelting('modern_industrialization:nickel_ingot', 'create:crushed_raw_nickel');
-  create.pressing()
-    .itemOut('modern_industrialization:tin_plate')
-    .itemIn('modern_industrialization:tin_ingot')
-    .id('craftoria:create/pressing/tin_ingot_to_tin_plate')
+
+  // added all MI plate to create press, exclude nuclear alloy, blastproof, iridium plate 
+  const materials = ['aluminum', 'annealed_copper', 'battery_alloy', 'beryllium', 'bronze',
+    'cadmium', 'chromium', 'cupronickel', 'electrum', 'invar', 'kanthal', 'lead', 'nickel', 
+    'platinum', 'silicon', 'silver', 'stainless_steel', 'steel', 'superconductor', 'tin', 
+    'titanium', 'tungsten']
+    
+  materials.forEach(material => {
+    create.pressing()
+      .itemOut(`modern_industrialization:${material}_plate`)
+      .itemIn(`modern_industrialization:${material}_ingot`)
+      .id(`craftoria:create/pressing/${material}_ingot_to_${material}`)
+  });
 });
