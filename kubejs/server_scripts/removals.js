871bafa2b6a6c62720f96ee7624c1341d718f9c7
AK <anthonkrisat@hotmail.com>
2026-09-06 10:24:14 +0200

    add back xycraft scripts

--- kubejs/server_scripts/removals.js
+++ kubejs/server_scripts/removals.js
@@ -20,6 +20,15 @@ const globalItemRemovals = [
   'utilitarian:tiny_charcoal',
   'pylons:infusion_pylon',
   'pylons:potion_filter',
+  'xycraft_world:raw_aluminum',
+  'xycraft_world:raw_aluminum_block',
+  'xycraft_world:aluminum_ore_stone',
+  'xycraft_world:aluminum_ore_deepslate',
+  'xycraft_world:aluminum_ore_kivi',
+  'xycraft_machines:aluminum_dirty_dust',
+  'xycraft_machines:aluminum_shard',
+  'xycraft_machines:aluminum_crystal',
+  'xycraft_machines:aluminum_clump',
   'create:crushed_raw_aluminum',
   'create:crushed_raw_platinum',
   'actuallyadditions:wooden_aiot',
@@ -58,12 +67,17 @@ ServerEvents.recipes(event => {
     'create:crushing/platinum_ore',
     'create:crushing/raw_platinum',
     'create:crushing/raw_platinum_block',
+    'xycraft_machines:compat/mek/compressor/aluminum_sheet_temp',
     'minecraft:blaze_rod_from_smelting_bronze_rod'
   ];
 
   /** @type {Special.Item[]} */
   const inputRemovals = [
-
+    'xycraft_world:raw_aluminum',
+    'xycraft_world:raw_aluminum_block',
+    'xycraft_world:aluminum_ore_stone',
+    'xycraft_world:aluminum_ore_deepslate',
+    'xycraft_world:aluminum_ore_kivi',
   ];
 
   id.forEach(id => {
@@ -107,6 +121,9 @@ ServerEvents.generateData('after_mods', event => {
   const miscYeets = [
     'dumplings_delight:loot_modifiers/add_calamari_squid',
     'apotheosis:affixes/armor/attribute/unbound',
+    'xycraft_world:worldgen/configured_feature/ore_aluminum',
+    'xycraft_world:worldgen/placed_feature/ore_aluminum',
+    'xycraft_world:neoforge/biome_modifier/ore_aluminum',
   ];
 
   /** @type {Special.LootTable[]} */
