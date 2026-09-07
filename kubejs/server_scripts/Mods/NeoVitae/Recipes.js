3936b05057d8f4e2f03b05fb5309fc2cf21da1dc
AK <anthonkrisat@hotmail.com>
2026-09-07 16:48:50 +0200

    feat(recipe): changed the recipe of Sigil of Temporal Dominance'

---
+++ kubejs/server_scripts/Mods/NeoVitae/Recipes.js
@@ -0,0 +1,7 @@
+ServerEvents.recipes(e => {
+
+      // Reagent Temporal Dominance
+      e.replaceInput({id: 'animusnv:hellfire_forge/reagenttemporaldominance'}, 'minecraft:echo_shard', 'justdirethings:time_crystal');
+      e.replaceInput({id: 'animusnv:hellfire_forge/reagenttemporaldominance'}, 'minecraft:clock', 'irons_spellbooks:timeless_slurry');
+
+});
