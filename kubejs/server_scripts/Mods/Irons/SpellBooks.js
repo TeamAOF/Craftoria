33fe03dc93db9e94148f7020230f5772a07410e2
WhitePhant0m <kallunki.aleksi@gmail.com>
2026-09-17 22:43:40 +0300

    upgrading spell capacity of the book now works

--- kubejs/server_scripts/Mods/Irons/SpellBooks.js
+++ kubejs/server_scripts/Mods/Irons/SpellBooks.js
@@ -1,16 +1,22 @@
 const $UpgradeUtils = Java.loadClass('io.redspace.ironsspellbooks.util.UpgradeUtils');
 const $UpgradeData = Java.loadClass('io.redspace.ironsspellbooks.api.item.UpgradeData');
+const $SpellContainer = Java.loadClass('io.redspace.ironsspellbooks.capabilities.magic.SpellContainer');
 
 // Special Recipe for upgrading the Codex of Evolution
 ServerEvents.recipes(event => {
   event.shapeless('craftoria:codex_of_evolution', [
     'craftoria:codex_of_evolution',
     '#craftoria:upgrade_orbs',
-  ]).modifyResult('codex_of_evolution').id('craftoria:codex_of_evolution_upgrade');
+  ]).modifyResult('codex_orb').id('craftoria:codex_of_evolution_orb_upgrade');
+
+  event.shapeless('craftoria:codex_of_evolution', [
+    'craftoria:codex_of_evolution',
+    'minecraft:paper',
+  ]).modifyResult('codex_slots').id('craftoria:codex_of_evolution_slots_upgrade');
 });
 
-// Recipe logic for the Codex of Evolution
-ServerEvents.modifyRecipeResult('codex_of_evolution', event => {
+// Recipe logic for the Codex of Evolution orb upgrades
+ServerEvents.modifyRecipeResult('codex_orb', event => {
   const { grid } = event;
   const ingredientCodex = grid.find('craftoria:codex_of_evolution');
   const ingredientOrb = grid.find('#craftoria:upgrade_orbs');
@@ -25,6 +31,19 @@ ServerEvents.modifyRecipeResult('codex_of_evolution', event => {
   event.success(result);
 });
 
+// Recipe logic for the Codex of Evolution spell slot upgrades
+ServerEvents.modifyRecipeResult('codex_slots', event => {
+  const { grid } = event;
+  const ingredientCodex = grid.find('craftoria:codex_of_evolution');
+  const result = ingredientCodex.copy();
+  const spellContainer = ingredientCodex.get('irons_spellbooks:spell_container').mutableCopy();
+  if (spellContainer.getMaxSpellCount() >= 15) event.cancel('minecraft:air');
+  spellContainer.setMaxSpellCount(spellContainer.getMaxSpellCount() + 1);
+  $SpellContainer.set(result, spellContainer.toImmutable());
+
+  event.success(result);
+});
+
 /** @type {Record<import("@package/io/redspace/ironsspellbooks/item/armor").$UpgradeOrbType_, number>} */
 const allowedAmounts = {
   // Example of allowed amounts for specific upgrade orbs
@@ -35,7 +54,7 @@ const allowedAmounts = {
 
 /**
  * Checks if an item can be upgraded with a specific upgrade orb.
- * @param {import("@package/net/minecraft/world/item").$ItemStack_} item
+ * @param {import('@package/net/minecraft/world/item').$ItemStack} item
  * @param {*} upgradeOrb
  * @return {boolean} True if the item can be upgraded with the given upgrade orb, false otherwise.
  */
