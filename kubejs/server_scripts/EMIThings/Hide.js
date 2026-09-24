c56b41a927d5c3f4d13d675b50a52027053a8bf4
WhitePhant0m <33400200+WhitePhant0m@users.noreply.github.com>
2026-09-24 04:11:39 +0300

    seems to have been fixed

--- kubejs/server_scripts/EMIThings/Hide.js
+++ kubejs/server_scripts/EMIThings/Hide.js
@@ -41,7 +41,6 @@ RecipeViewerEvents.removeCategories(e => {
     'chisel:chisel_recipes_category',
     'modern_industrialization:replicator_1',
     'ftbquests:loot_crate',
-    'create_dragons_plus:ending', // Currently always errors out when trying to display, will unhide when fixed
     'mekmm:replicator',
     'mekmm:fluid_replicator',
     'mekmm:chemical_replicator',
